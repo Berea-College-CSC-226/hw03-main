08b9a4c37e368029750f36098ec6c10f0d3bbb42
Anderson <stettnera@berea.edu>
2018-09-20 19:06:30 -0700

    A03 Anderson Stettner

--- A03.py
+++ A03.py
@@ -1,6 +1,5 @@
 #################################################
 # Anderson Stettner A03
-
 #https://docs.google.com/document/d/1-WyeAyLQg33YSv7AE61My-hWb5yDrYVNEBUImbhSZU8/edit?usp=sharing
 #################################################
 import turtle
