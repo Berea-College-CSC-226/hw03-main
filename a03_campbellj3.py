fd5439dcaaa9d59c5d1820d2cc5a36078644b5fe
campbellj3 <campbellj3@berea.edu>
2019-09-17 18:04:02 -0400

    Finished a03, left credits in the comment block.

--- a03_campbellj3.py
+++ a03_campbellj3.py
@@ -9,23 +9,39 @@
 # Modeled after the house code in t03. Credit to Dr. Scott Heggen and Emily Lovell
 ########################################################################################################################
 
+# Author: Jeremy Campbell
+
+# Username: campbellj3
+
+# Assignment: a03
+
+# Purpose: Design a moderately complex image that utilizes a rgb color range.
+
+# Acknowledgements: Received assistance from Aziz, and Dr. Heggen.
+
+# Modeled after the house code in t03. Credit to Dr. Scott Heggen and Emily Lovell.
+# Source: https://www.mouseplanet.com/12222/Disney_Goes_to_Hell_for_Halloween
+########################################################################################################################
+
 import turtle  # opens the turtle library
 
 
-def make_roof(wn, bob):
-    """
-    Constructing the roof of the house.
+# def make_roof(wn, bob):
+    # Constructing the roof of the house.
 
-    :param wn: the turtle screen object
-    :param bob: the turtle object we'll be using
-    :return: No specified return
-    """
+    # :param wn: the turtle screen object
+    # :param bob: the turtle object we'll be using
+    # :return: No specified return
+
     # wn.register_shape("roof.png")
     # bob.penup()
     # bob.setpos(45, 45)
+
+
     # bob.pendown()
     # wn.shape("roof.png")
     # bob.stamp()
+
 
 def make_main_house(bob):
     """
@@ -35,15 +51,16 @@ def make_main_house(bob):
     :return: N/A
     """
     bob.penup()
-    bob.setpos(30, 47)
+    bob.setpos(35, 47)
     bob.pendown()
-    bob.color('brown')
+    bob.color('dark red')
     bob.begin_fill()
     for side in range(2):
         bob.forward(100)
         bob.right(90)
         bob.forward(140)
         bob.right(90)
+
     bob.end_fill()
 
 
@@ -77,7 +94,7 @@ def make_door(bob):
     :return: N/A.
     """
     bob.penup()
-    bob.setpos(70, -42)
+    bob.setpos(75, -42)
     bob.pendown()
     bob.color('black')
     bob.begin_fill()
@@ -98,8 +115,8 @@ def make_text(bob, txt):
     :return: N/A.
     """
     bob.penup()
-    bob.color('dark red')
-    bob.setpos(70, 210)
+    bob.color('black')
+    bob.setpos(70, 200)
     bob.write(txt, move=False, align='center', font=("Arial", 25, ("bold", "normal")))
 
 
@@ -112,17 +129,17 @@ def main():
     #turtle.colormode(255)
 
     wn = turtle.Screen()
-    wn.bgpic("DH.PNG")
+    wn.bgpic("DHRoof6.png")
     bob = turtle.Turtle()
     bob.hideturtle()
 
     # Will call the defined functions
-    make_roof(wn, bob)
+    # make_roof(wn, bob)
     make_main_house(bob)
-    make_window(bob, 45, 0)
-    make_window(bob, 88, 0)
+    make_window(bob, 50, 0)
+    make_window(bob, 93, 0)
     make_door(bob)
-    make_text(bob, "At least the rents cheap...")
+    make_text(bob, "At least the rents cheap...")   # Leaves text on the screen.
 
     wn.exitonclick()  # allows user to exit the ran program as wanted.
 
