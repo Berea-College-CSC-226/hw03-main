6c6c712c74262d3f21fb4dcb3176dd40eb857501
Atu175 <atub@berea.edu>
2020-02-05 21:16:34 -0500

    Finished code for A03

--- a03_atu.py
+++ a03_atu.py
@@ -7,7 +7,7 @@
 # Google Doc Link: https://docs.google.com/document/d/1c5eu4vL2XOw1urT8wXGVSEBSlOtsq3Ih0KEi_JZgfmU/edit?usp=sharing
 #
 #################################################################################
-# Acknowledgements:
+# Acknowledgements: Used the code for writing labels from a02
 #
 #
 #################################################################################
@@ -17,42 +17,29 @@ import turtle
 turt = turtle.Turtle()
 
 
-def main():
+def write_title():    # Writing thr title under the diagram
     """
-    Docstring for main
+    Writing the title
+    :return: None
     """
-    # ...
-    wn = turtle.Screen()
-    wn.bgcolor('grey')
-    head = 0
-    turt.pensize(5)
-    build_body(turt)
-    draw_door(turt)
-    draw_window(turt)
-    draw_roof(turt)
-    draw_moon()
-    draw_tree()
-    write_title()
-    turt.hideturtle()
-
-    wn.exitonclick()
-
-
-def write_title():
     turt.penup()
     turt.goto(0, -200)
     turt.pendown()
-    turt.color('black')
+    turt.color('#000000')
     turt.left(-90)
     turt.write("Home Sweet Home", move=False, align='center', font=("Arial", 32, ("bold", "normal")))
 
 
-def draw_tree():
+def draw_tree():         # Drawing a tree beside the house
+    """
+
+    :return: None
+    """
     turt.penup()
     turt.goto(-300, -100)
-    turt.pendown
+    turt.pendown()
     turt.right(-120)
-    turt.color('brown')
+    turt.color('#9C661F')
     turt.begin_fill()
     for i in range(2):
         turt.forward(10)
@@ -65,26 +52,35 @@ def draw_tree():
     turt.left(-90)
     turt.forward(100)
     turt.left(90)
-    turt.color('green')
+    turt.color('#008000')
     turt.begin_fill()
     turt.circle(100, 360)
     turt.end_fill()
 
 
-def draw_moon():
+def draw_moon():                 # Drawing the moon
+    """
+
+    :return: None
+    """
     turt.left(60)
     turt.penup()
     turt.goto(250, 280)
     turt.pendown()
     turt.left(60)
-    turt.color('white')
+    turt.color('#FFFFFF')
     turt.begin_fill()
     turt.circle(50, 180)
     turt.end_fill()
 
 
-def draw_roof(turt):
-    turt.color('black')
+def draw_roof(turt):                  # Drawing the roof
+    """
+
+    :param turt: Drawing the roof
+    :return: None
+    """
+    turt.color('#000000')
     turt.penup()
     turt.goto(-150, 100)
     turt.pendown()
@@ -98,7 +94,12 @@ def draw_roof(turt):
     turt.end_fill()
 
 
-def draw_window(turt):
+def draw_window(turt):  # Drawing the window
+    """
+
+    :param turt: Drawing the window
+    :return: None
+    """
     turt.penup()
     turt.goto(55, 40)
     turt.pendown()
@@ -115,7 +116,12 @@ def draw_window(turt):
 
 
 def draw_door(turt):
-    turt.color('blue')
+    """
+
+    :param turt: Drawing the door
+    :return: None
+    """
+    turt.color('#0000FF')
     turt.penup()
     turt.forward(120)
     turt.pendown()
@@ -130,10 +136,16 @@ def draw_door(turt):
 
 
 def build_body(turt):
+    """
+
+    :param turt: Building the body
+    :return: None
+
+    """
     turt.penup()
     turt.goto(-150, -100)
     turt.pendown()
-    turt.color('red')
+    turt.color('#FF0000')
     turt.begin_fill()
     for i in range(2):
         turt.forward(300)
@@ -141,6 +153,28 @@ def build_body(turt):
         turt.forward(200)
         turt.left(90)
     turt.end_fill()
+
+
+def main():
+    """
+    This enables  the other functions to function
+
+    """
+    # ...
+    wn = turtle.Screen()
+    wn.bgcolor('#808080')
+    head = 0
+    turt.pensize(5)
+    build_body(turt)
+    draw_door(turt)
+    draw_window(turt)
+    draw_roof(turt)
+    draw_moon()
+    draw_tree()
+    write_title()
+    turt.hideturtle()
+
+    wn.exitonclick()
 
 
 main()
