dd949be869963a83605534ae0ed9998f4183f4d6
whited3 <whited3@berea.edu>
2023-09-13 20:35:21 -0400

    made my branch and added my heading

---
+++ hw03_whited3.py
@@ -0,0 +1,13 @@
+#################################################################################
+# Author: Destiny White
+# Username: whited3
+#
+# Assignment:HW03- Fully Functional GItty Psychedelic Turtles
+# Purpose: To learn more about git, functions, and doc strings
+# Google Doc Link: https://docs.google.com/document/d/1JwFWjoXfjxo1U_hnxmwLi8dFDfw2O1sIfiP6n8Y5bWw/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
