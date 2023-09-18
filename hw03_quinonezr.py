1d9c2b700905bc220590ab0819c88279428ddb1e
Ruben Quinonez <quinonezr@berea.edu>
2023-09-17 23:46:57 -0400

    Created hw file and branch

---
+++ hw03_quinonezr.py
@@ -0,0 +1,43 @@
+#################################################################################
+# Author:Ruben Quinonez
+# Username:quinonezr
+#
+# Assignment:HW03 Fully Functional Gitty Turtles
+# Purpose:
+# Google Doc Link:https://docs.google.com/document/d/1FCB9KRG7pKzSgf5R-j-xW5jRYbEhy0kJC9hHV7eF9No/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+import turtle
+
+
+def function_1():
+    """
+    Docstring for function_1
+    """
+    pass
+    # ....
+
+
+def function_2():
+    """
+    Docstring for function_2
+    """
+    pass
+    # ...
+
+
+def main():
+    """
+    Docstring for main
+    """
+    # ...
+    function_1()            # Function call to function_1
+    function_2()            # Function call to function_2
+
+
+main()
