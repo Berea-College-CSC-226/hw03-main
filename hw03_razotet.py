b6f4eef2e0e2fdc47a26beead0018ea0ad1aec06
razotet <razotet@berea.edu>
2023-09-18 11:04:15 -0400

    I made a house and fixed my indentation

--- hw03_razotet.py
+++ hw03_razotet.py
@@ -3,11 +3,17 @@
 # Username:razotet
 #
 # Assignment:HW03-Fully Functional Gitty Psychedelic Robotic Turtles
+
 # Purpose:
+Continue practicing creating and using functions.
+More practice on using the turtle library.
+Learn about how computers represent colors.
+Learn about source control and Git.
+
 # Google Doc Link:https://docs.google.com/document/d/1o3qQdqf5zXFRLx-ZWTtfvhRjhSI-q1dkjrt7Ms-U91M/edit?usp=sharing
 
 #################################################################################
-# Acknowledgements:
+# Acknowledgements: Dr. Scott Heggen
 #
 #
 #################################################################################
@@ -17,7 +23,7 @@ import turtle
 
 def house(tur_house):
     """
-This uses a turtle to create the general shape of a house.
+    This uses a turtle to create the general shape of a house.
     """
     tur_house.speed(0)
     tur_house.pensize(10)
