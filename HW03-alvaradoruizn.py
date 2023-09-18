3ef2e644a9de32588a9b62b661ef44203b535572
alvaradoruizn <alvaradoruizn@berea.edu>
2023-09-18 18:29:08 -0400

    made changes to HW03 and created a house drawing

--- HW03-alvaradoruizn.py
+++ HW03-alvaradoruizn.py
@@ -1,34 +1,59 @@
 
 #################################################################################
-# Author:
-# Username:
+# Author: Nancy Alvarado Ruiz
+# Username:alvaradoruiz
 #
-# Assignment:
-# Purpose:
-# Google Doc Link:
+# Assignment:HW03
+# Purpose: Create a complex drawing
+# Google Doc Link:https://docs.google.com/document/d/1MJcOBvwG28zuDBP83rLQiCzc2plLyxOdp0uERAAff4o/edit?usp=sharing
 #
 #################################################################################
 # Acknowledgements:
 #
 #
 #################################################################################
-
 import turtle
-
-
-def function_1():
+def setupWindow(square):
     """
-    Docstring for function_1
+    Set up screen to draw square part of house
     """
-    pass
-    # ....
+    square.penup()
+    square.setpos(-150,-150)
+    square.pendown()
+    square.pensize(17)
+    square.color("pink")
 
+def setup_window(triangle):
+    triangle.penup()
+    triangle.setpos(-150,-15)
+    triangle.pendown()
+    triangle.pensize(17)
+    triangle.color("light blue")
+def setupwindow(door):
+    door.penup()
+    door.setpos(-70,-140)
+    door.pendown()
+    door.pensize(10)
+    door.color("yellow")
 
-def function_2():
+def makeDoor(t):
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.forward(30)
+    t.left(90)
+    t.forward(50)
+def makeTriangle(t,sz):
+    for i in range(3):
+        t.forward(sz)
+        t.left(sz)
+def makesqaure(t,sz):
     """
-    Docstring for function_2
+    creates square
     """
-    pass
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
     # ...
 
 
@@ -36,9 +61,18 @@ def main():
     """
     Docstring for main
     """
-    # ...
-    function_1()            # Function call to function_1
-    function_2()            # Function call to function_2
+    wn = turtle.Screen()
+    wn.bgcolor("dark blue")
+    square = turtle.Turtle()
+    triangle = turtle.Turtle()
+    door = turtle.Turtle()
+    setupWindow(square)
+    setup_window(triangle)# Function call to function_1
+    setupwindow(door)
+    makesqaure(square,120)
+    makeTriangle(triangle,120)
+    makeDoor(door)
+    wn.exitonclick() # Function call to function_2
 
 
 main()
