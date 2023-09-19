0e538fd5537deab1b269be805a67b72f6e5b0570
karimh <karimh@berea.edu>
2023-09-19 18:52:55 -0400

    Made a new local branch and made my typed up the basic framework of my code.

---
+++ hw03_karimh.py
@@ -0,0 +1,26 @@
+
+# Author: Hira Karim
+# Username: karimh
+#
+# Assignment: HW03-Fully Functional Gitty Psychedelic Robotic Turtle
+# Purpose: To create a code using functions, and submit a pull request.
+# Google Doc Link: https://docs.google.com/document/d/12MLXaEkr6jyzp5jVL2UXoEJTj2VyV12LwCJIk-eW6fs/edit?usp=sharing
+
+import turtle
+
+def function_1():
+    "include docstring"
+
+def function_2():
+    "docstring"
+
+def function_3():
+    "docstring"
+
+def fucntion_4():
+    "docstring"
+
+def main ():
+    #call functions
+
+    main()
