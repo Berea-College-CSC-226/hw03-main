e3cbc36c5d9b388f941e2b011cd07b6cd0eae393
whited3 <whited3@berea.edu>
2023-09-20 09:44:51 -0400

    hehe

--- hw03_whited3.py
+++ hw03_whited3.py
@@ -14,11 +14,12 @@
 
 import turtle
 
-def mb():
+def main_body(spe):
     """
     Creating the main building part of my house
     """
     mb = turtle.Turtle()
+    mb.speed(spe)
     mb.ht()
     mb.up()
     mb.setpos(-100, -100)
@@ -39,7 +40,7 @@ def mb():
     mb.pencolor('royal blue')       # STUCCO
     mb.fillcolor('royal blue')
     mb.begin_fill()
-    for f in range(2):      ##TODO: Change placeholder colors
+    for i in range(2):      ##TODO: Change placeholder colors
         mb.fd(100)
         mb.right(90)
         mb.fd(200)
@@ -60,8 +61,9 @@ def mb():
     mb.fd(200)
     mb.end_fill()
 
-def chim():
+def chimney(spe):
     ch = turtle.Turtle()
+    ch.speed(spe)
     ch.pencolor('yellow')
     ch.ht()
     ch.up()
@@ -80,24 +82,104 @@ def chim():
     ch.fd(60)
     ch.end_fill()
 
-def extension():
+def extension(spe):
     ex = turtle.Turtle()
+    ex.speed(spe)
+    ex.ht()
+    ex.up()
+    ex.setpos(20, -100)
+    ex.down()
+    ex.fillcolor("purple")
+    ex.begin_fill()
     for i in range(2):
-        ex.fd(40)
+        ex.fd(80)
         ex.left(90)
-        ex.fd(100)
+        ex.fd(150)
         ex.left(90)
+    ex.end_fill()
+    ex.left(90)
+    ex.fd(150)
+    ex.begin_fill()
+    ex.right(50)
+    ex.fd(58)
+    ex.right(85)
+    ex.fd(52)
+    ex.right(135)
+    ex.fd(60)
+    ex.end_fill()
 
+def window(spe):
+    bay = turtle.Turtle()
+    bay.speed(spe)
+    bay.ht()
+    bay.up()
+    bay.setpos(-80,20)
+    bay.down()
+    bay.fillcolor('black')
+    bay.begin_fill()
+    for i in range(2):
+        bay.fd(80)
+        bay.right(90)
+        bay.fd(50)
+        bay.right(90)
+    bay.end_fill()
 
+    bay.pensize(5)
+    bay.pencolor("#5B99CE")     # brown
+    for i in range(2):
+        bay.fd(80)
+        bay.right(90)
+        bay.fd(50)
+        bay.right(90)
+    bay.fd(40)
+    bay.right(90)
+    bay.fd(50)
+    bay.right(90)
 
+def door(spe):
+    door = turtle.Turtle()
+    door.ht()
+    door.speed(1)
+    door.up()
+    door.setpos(30, -100)
+    door.down()
+    door.fillcolor("#5B99CE")   # brown
+    door.begin_fill()
+    door.left(90)
+    door.fd(100)
+    door.circle(-30, 180)
+    door.fd(100)
+    door.right(90)
+    door.fd(70)
+    door.end_fill()
+
+def background(spe):
+    bg = turtle.Turtle()
+    bg.speed(spe)
+    bg.fillcolor("#5BCE5E")    # blue
+    bg.begin_fill()
+    bg.up()
+    bg.setpos(-200,-100)
+    bg.up()
+    for i in range(2):
+        bg.fd(400)
+        bg.left(90)
+        bg.fd(400)
+        bg.left(90)
+    bg.end_fill()
 
 def main():
     wn = turtle.Screen()
+    wn.bgcolor("#5BCE5E")   # green
     wn.setup(400, 400)
 
-    # chim()
-    # mb()
-    #extension()
+    spe = 0         #turtle speed
+    background(spe)
+    chimney(spe)
+    main_body(spe)
+    extension(spe)
+    window(spe)
+    door(spe)
     wn.exitonclick()
 
 main()
