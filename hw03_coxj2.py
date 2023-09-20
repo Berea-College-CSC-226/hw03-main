5b090072c1dc2272ce8bc0037a5378d79d5be39c
Johncox2211 <johncox428@yahoo.com>
2023-09-20 11:46:46 -0400

    fixed PEP8 (i think)

--- hw03_coxj2.py
+++ hw03_coxj2.py
@@ -11,6 +11,7 @@
 import turtle
 import random
 
+
 def drawRectangle(t, l, w):
     """
     Draw a Rectangle at current position with 't' turtle
@@ -21,21 +22,26 @@ def drawRectangle(t, l, w):
         t.right(90)
         t.fd(w)
         t.right(90)
+
+
 def drawSquare(t, l):
     """
     Draws a square at current position using 't' turtle with 'l' side length
     """
     drawRectangle(t, l, l)
+
+
 def drawTriangle(t, x1, y1, x2, y2, x3, y3):
     """
     Draws a triangle starting from x1 and y1
     size and length depends on how far apart coordinates are
     (accepts a turtle name and three x, y coordinates)
     """
-    t.goto(x1,y1)
+    t.goto(x1, y1)
     t.goto(x2, y2)
     t.goto(x3, y3)
     t.goto(x1, y1)
+
 
 def move(t, x, y):
     """
@@ -45,6 +51,7 @@ def move(t, x, y):
     t.goto(x, y)
     t.pendown()
 
+
 def drawProceduralTriangles(t):
     """
     draw random sized triangles procedurally in a sort of grid pattern with 't' turtle
@@ -53,23 +60,23 @@ def drawProceduralTriangles(t):
     y = -250
     move(t, x, y)
     i = 0
-    while(i < 5):
+    while (i < 5):
         i += 1
         move(t, x, y)
-        drawTriangle(t,x ,y , random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20), random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20))
+        drawTriangle(t, x, y, random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20),
+                     random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20))
 
         x += 80
         y = -250
         j = 0
 
-        while(j < 5):
+        while (j < 5):
             j += 1
             move(t, x, y)
-            drawTriangle(t,x ,y , random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20), random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20))
+            drawTriangle(t, x, y, random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20),
+                         random.randrange(x - 40, x + 40), random.randrange(y - 20, y + 20))
 
             y += 80
-
-
 
 
 def main():
