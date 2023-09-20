acb68100b239d15fd42d3bd12e9fe4cec99dc126
adhikaris <adhikaris@berea.edu>
2023-09-20 14:22:16 -0400

    done with cat face.

--- hw03_adhikaris.py
+++ hw03_adhikaris.py
@@ -49,19 +49,42 @@ def draw_eyes(cat, x,y):
     cat.forward(100)
     cat.pendown()
     cat.circle(-25)
-    cat.forward(120)
-    cat.right(-85)
+    #cat.forward(120)
+    #cat.right(-85)
     cat.end_fill()
 
+def draw_ear(cat, x,y):
+    cat.begin_fill()
+    cat.color("white")
+    cat.penup()
+    cat.goto(-70, 70)  # Position the turtle
+    cat.pendown()
+    for i in range(3):
+        cat.forward(50)
+        cat.left(120)
+    cat.end_fill()
 
+def right_ear(cat, x,y):
+    cat.begin_fill()
+    cat.color("white")
+    cat.penup()
+    cat.goto(20, 70)
+    cat.pendown()
+    for i in range(3):
+        cat.forward(50)
+        cat.left(120)
+    cat.end_fill()
 
-
-
-
-
-
-
-
+def draw_mouth(cat, x,y):
+    cat.penup()
+    cat.goto(-55, -55)  # Position the turtle
+    cat.pendown()
+    cat.circle(radius=60, extent=80)
+    cat.right(20)
+    cat.left(-20)
+    cat.pendown()
+    cat.right(120)
+    cat.circle(radius=60, extent=80)
 
 
 def main():
@@ -74,10 +97,12 @@ def main():
     cat.speed(10)
     cat.goto(0,50)
     draw_eyes(cat, 70,70)
-    cat.goto(0,0)
-    cat.forward(-85)
-    cat.right(80)
-    cat.circle(radius=80,extent=170)
+    draw_ear(cat, -70, 70)
+    draw_mouth(cat, -40, 40)
+    right_ear(cat, 20, 70)
+    #cat.forward(-85)
+    #cat.right(80)
+    #cat.circle(radius=80,extent=170)
     # triangle(70,70)
     # triangle(-70, 70)
     # draw_body(0,0)
@@ -108,7 +133,7 @@ def main():
     Docstring for main
     """
     # ...
-    cat.hideturtle()
+    #cat.hideturtle()
     wn.exitonclick()
 
 main()
