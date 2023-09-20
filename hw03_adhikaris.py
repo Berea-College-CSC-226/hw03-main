04da8ab6ada33438ca46b5909aa30e8e2c73a4a2
adhikaris <adhikaris@berea.edu>
2023-09-20 01:15:02 -0400

    made cat body but still working on the ears and mouth.

--- hw03_adhikaris.py
+++ hw03_adhikaris.py
@@ -4,6 +4,7 @@
 import turtle
 cat = turtle.Turtle()
 wn = turtle.Screen()
+side_length= 50
 
 # Draw the cat's head
 
@@ -17,18 +18,46 @@ def draw_head(x,y):
     cat.circle(100)
     cat.end_fill()
 
+def draw_body(x,y):
+    """ This function is suppose to draw the head of a cat"""
+    cat.penup()
+    cat.goto(0, -140)
+    cat.pendown()
+    cat.begin_fill()
+    cat.color("black")
+    cat.circle(120)
+    cat.end_fill()
+
+
+
+def triangle(x,y):
+    cat.begin_fill()
+    cat.color("black")
+    cat.forward(50)
+    for i in range(3):
+        cat.forward(side_length)
+        cat.left(120)
+    cat.forward(120)
+    cat.right(-85)
+    cat.end_fill()
+
+
 def draw_eyes(x,y):
     cat.begin_fill()
     cat.color("white")
     cat.penup()
     cat.forward(-50)
     cat.pendown()
-    cat.circle(-15)
+    cat.circle(-25)
     cat.penup()
     cat.forward(100)
     cat.pendown()
-    cat.circle(-15)
+    cat.circle(-25)
     cat.end_fill()
+
+
+
+
 
 
 
@@ -45,9 +74,24 @@ def main():
     cat.goto(0,50)
     draw_eyes(70,70)
     cat.goto(0,0)
-    cat.forward(-80)
-    cat.left(-90)
+    cat.forward(-85)
+    cat.right(80)
     cat.circle(radius=80,extent=170)
+    triangle(70,70)
+    triangle(-70, 70)
+    draw_body(0,0)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -66,6 +110,7 @@ def main():
 
 
 main()
+cat.hideturtle()
 wn.exitonclick()
 
 
