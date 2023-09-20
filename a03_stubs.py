831d248cfa87552ee730e08ccbe4b578d935ea05
Scott Heggen <heggens@berea.edu>
2023-09-20 10:49:41 -0400

    Revert "imran cemetery with ghost"
    This reverts commit 39bcc1d5

--- a03_stubs.py
+++ a03_stubs.py
@@ -14,3 +14,34 @@
 #
 #################################################################################
 
+import turtle
+
+
+def function_1():
+    """
+    Docstring for function_1
+    """
+    pass
+    # ....
+
+
+def function_2():
+    """
+    Docstring for function_2
+    """
+    pass
+    # ...
+
+
+def main():
+    """
+    Docstring for main
+    """
+    # ...
+    function_1()            # Function call to function_1
+    function_2()            # Function call to function_2
+
+
+main()
+
+
