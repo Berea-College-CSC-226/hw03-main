63f56e5e57fdb264512176ca27d82b7919a76c2e
oguinef <oguinef@berea.edu>
2023-09-20 08:55:19 -0400

    HW assignment added

---
+++ hw03_oguinef.py
@@ -0,0 +1,39 @@
+#################################################################################
+# Author: Frederick Oguine
+# Username: oguinef
+# Google Doc: https://docs.google.com/document/d/1CtGVE73xZCMP0tAIBb7fdBZGuHK2BC2an2vo10VEUag/edit?usp=sharing
+# Assignment: HW03
+# Purpose:
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+import turtle
+
+# Define colors
+fRED = (34, 123, 230)
+
+
+def draw_background():
+    """"""
+    turtle.bgcolor(fRED)
+
+
+def draw_main_object():
+    """"""
+
+
+def add_details():
+    """"""
+
+
+def main():
+    draw_background()
+    draw_main_object()
+    add_details()
+    turtle.done()  # This ends the turtle graphics process
+
+
+main()
