e6979e4925e265a04bcbeeef9a0e6c68f097aaf0
dorcenak <dorcenak@berea.edu>
2023-09-25 21:11:08 -0400

    Formatted the doctrines.

--- hw03_dorcenak.py
+++ hw03_dorcenak.py
@@ -18,7 +18,13 @@
 import turtle               # allows us to use the turtles library
 
 
-def make_roof(wn, shape):                 #  Create the roof of the house
+def make_roof(wn, shape):
+    '''
+    Create the roof of the house.
+
+    Returns:
+        None
+    '''
     wn.register_shape("top.gif")         # Registers the shape that must be used by turtle library
     shape.penup()
     shape.setpos(120, 80)
@@ -26,7 +32,13 @@ def make_roof(wn, shape):                 #  Create the roof of the house
     shape.shape("top.gif")               # Sets the shape to the image registered above
     shape.stamp()
 
-def make_main_house(shape):                 #   Makes the main house rectangle.
+def make_main_house(shape):
+    '''
+    Makes the main house rectangle.
+
+    Returns:
+        None
+    '''
     shape.setpos(1, 30)
     shape.color('#3333FF')
     shape.begin_fill()
@@ -38,7 +50,13 @@ def make_main_house(shape):                 #   Makes the main house rectangle.
     shape.end_fill()
 
 
-def make_window(shape, x, y):       # Adds a window to the house.
+def make_window(shape, x, y):
+    '''
+    Adds a window to the house.
+
+    Returns:
+        None
+    '''
     shape.penup()
     shape.setpos(x, y)
     shape.pendown()
@@ -52,7 +70,13 @@ def make_window(shape, x, y):       # Adds a window to the house.
     shape.end_fill()
 
 
-def make_door(shape):           #  Adds a door to the house.
+def make_door(shape):
+    '''
+    Adds a door to the house.
+
+    Returns:
+        None
+    '''
     shape.penup()
     shape.setpos(70, -60)
     shape.pendown()
@@ -66,7 +90,13 @@ def make_door(shape):           #  Adds a door to the house.
     shape.end_fill()
 
 
-def make_flower(wn, shape):         #   Adds flowers in the front of the house.
+def make_flower(wn, shape):
+    '''
+    Adds flowers in the front of the house.
+
+    Returns:
+        None
+    '''
     wn.register_shape("flower.gif")
     shape.penup()
     shape.setpos(110, -170)
@@ -75,13 +105,25 @@ def make_flower(wn, shape):         #   Adds flowers in the front of the house.
     shape.up()
 
 
-def make_text(shape, txt):          #   Writes text to the screen.
+def make_text(shape, txt):
+    '''
+    Writes text to the screen.
+
+    Returns:
+        None
+    '''
     shape.color("#0F00F0")
     shape.setpos(70,140)
     shape.write(txt, move=False, align='center', font=("Arial", 30, ("bold", "normal")))
 
 
-def main():         # Draws a house at x, y on the screen.
+def main():
+    '''
+    Draws a house at x, y on the screen.
+
+    Returns:
+        None
+    '''
     turtle.colormode(255)           # change color modes
 
     wn = turtle.Screen()            # Makes a new turtle screen
