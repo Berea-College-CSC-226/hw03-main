ffb7a92596da719d27d186b7f9df8c52ecffb628
carnesa <carnesa@berea.edu>
2024-02-02 10:31:30 -0500

    Added header section and created hw03_carnesa.py.

---
+++ hw03_carnesa.py
@@ -0,0 +1,47 @@
+#################################################################################
+# Author: Andrew Carnes
+# Username: Carnesa
+#
+# Assignment: HW03: Fully Functional Gitty Psychedelic Robotic Turtles
+# Purpose: To understand RGB colors and their application to the turtle library.
+# To understand and implement both general functions and the main function and follow the order of operations depicted
+# in the homework.
+# To understand GitHub and Git terminology, how to create branches, committing, pushing, pulling, and merging.
+# Google Doc Link: https://docs.google.com/document/d/1vnz_cg1XCmgXAm4LlyNUGNvoOjfZNXkhbUEyUmNuhYs/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+# Block header and function outline copied from hw03_stub.py by Scott Heggan
+#
+#################################################################################
+
+import turtle
+
+
+def function_1():
+    """
+    Example docstring for function_1. function_1 is not a good function name and should be changed.
+    """
+    pass
+    # ....
+
+
+def function_2():
+    """
+    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    """
+    pass
+    # ...
+
+
+def main():
+    """
+    Docstring for main. Should describe the main functionality of this file.
+    """
+
+    # Function calls to function_1 and function_2.
+    function_1()            # TODO  Remove when you replace it with your function
+    function_2()            # TODO  Remove when you replace it with your function
+
+
+main()  # Starts the program!
