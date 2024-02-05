18f999128e358cedf354b1b1cc967b8f9577f03e
MalakAhmedSaad <malkahmedsaad2005@gmail.com>
2024-02-04 22:13:51 -0500

    changed the background and added a pattern function

--- hw03_mohamedm.py
+++ hw03_mohamedm.py
@@ -6,7 +6,7 @@
 # Purpose: exploring turtles
 # Google Doc Link: https://docs.google.com/document/d/1y-CIy7MdTK91WeMf1-7qtPrdj9Rpr0n_j_vf17A3Qe8/edit?usp=sharing
 #
-# Acknowledgements:
+# Acknowledgements: https://stackoverflow.com/questions/29441237/how-to-draw-a-semicircle-in-python-turtle-only  used for doing the pattern function
 #
 #################################################################################
 
@@ -19,7 +19,7 @@ def drawRectangle(turtle,x,y):
     turtle.penup()
     turtle.setpos(x, y)
     turtle.pendown()
-    turtle.color('hotpink')
+    turtle.color('#EC3434')
     turtle.begin_fill()
     for side in range(2):
         turtle.forward(420)
@@ -39,7 +39,7 @@ def drawWindow(turtle, x, y):
         turtle.penup()
         turtle.forward(50)
         turtle.pendown()
-        turtle.color('purple')
+        turtle.color('#2911A2')
         turtle.begin_fill()
 
         for side in range(2):
@@ -57,7 +57,7 @@ def drawDoor(turtle,x,y):
     turtle.penup()
     turtle.setpos(x, y)
     turtle.pendown()
-    turtle.color('purple')
+    turtle.color('#2911A2')
     turtle.begin_fill()
     for side in range(2):
         turtle.forward(320)
@@ -70,19 +70,31 @@ def drawText(turtle, txt,x,y):
         this function draws the text
         """
     turtle.penup()
-    turtle.color("#A11551")
+    turtle.color("#070418")
     turtle.setpos(x,y)
     turtle.forward(130)
     turtle.pendown()
     turtle.write(txt, move=False, align='center', font=("Times New Roman", 70, ("bold", "normal")))
 
-
+def pattern(turtle):
+    """
+            this function the pattern at the end
+            """
+    turtle.penup()
+    turtle.forward(700)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.pendown()
+    for i in range(30):
+        turtle.circle(100, 180)
+        turtle.circle(100//2, 180)
+        turtle.forward(1)
 def main():
     turtle.colormode(255)
 
     wn = turtle.Screen()
-    wn.bgcolor("#FFE5FD")
-    wn.bgpic("R.gif")
+    wn.bgcolor("#87E5E8")
+    wn.bgpic("R (1).gif")
     malak = turtle.Turtle()
     tess = turtle.Turtle()
     malak.speed(0)
@@ -98,6 +110,7 @@ def main():
     drawText(malak,"company",290,150)
     drawText(malak, "Malak's", -600, 150)
     drawText(tess, "Gimme the money!!!", -80, -280)
+    pattern(tess)
     wn.exitonclick()
 
 
