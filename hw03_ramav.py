fd5f0be8627072f9444bbac6587f48f3edaba620
ramav <valentinrama18@gmail.com>
2024-02-05 19:39:57 -0500

    Created file, and testing commit without push in TA hours

---
+++ hw03_ramav.py
@@ -0,0 +1,46 @@
+#################################################################################
+# Author:Valentin Rama
+# Username: ramav
+#
+# Assignment: HW03
+# Purpose: Creating a cat
+# Google Doc Link: https://docs.google.com/document/d/18V8i2I56Gj5T8IJGo4s0cEIvQE2YgXbE2uGVO0n7EVE/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+import turtle
+
+
+def function_1():
+    """
+    Example docstring for function_1. function_1 is not a good function name and should be changed.
+    """
+    pass
+    # ....
+
+
+def function_2():
+    """
+    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    """
+    pass
+    # ...
+
+
+def main():
+    """
+    Docstring for main. Should describe the main functionality of this file.
+    """
+
+    # Function calls to function_1 and function_2.
+    function_1()            # TODO  Remove when you replace it with your function
+    function_2()            # TODO  Remove when you replace it with your function
+
+
+main()  # Starts the program!
+
+
