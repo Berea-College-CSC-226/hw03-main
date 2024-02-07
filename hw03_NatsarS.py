df2e5ea60b0fa19b772b8b5b2afff88a27677194
Salaam Nahasi <nahasis@berea.edu>
2024-02-07 18:37:18 -0500

    reformated code to proper sections in main, flowercenter, and petals functions.Also changed coordinates to center of sunflower bulb so that it would be better centered.

--- hw03_NatsarS.py
+++ hw03_NatsarS.py
@@ -10,34 +10,43 @@
 # https://stackoverflow.com/questions/32804572/how-to-hide-a-turtle-icon-pointer-in-python
 #
 #################################################################################
-
 import turtle
-sunflower = turtle.Turtle()
-sunflower.speed(0)
-s=turtle.Screen()
-s.bgcolor('light green')
+
 
 def main():
-        """
-        Create the center bulb to our flower!
-        """
-flowercenter = turtle.Turtle()
-flowercenter.hideturtle()
-flowercenter.speed(0)
-flowercenter.goto(-5,-65)
-flowercenter.color("#503D02")
-flowercenter.begin_fill()
-flowercenter.circle(50)  # draws a circle of radius 20
-flowercenter.end_fill()
+    sunflower = turtle.Turtle()
+    sunflower.speed(0)
+    s = turtle.Screen()
+    s.bgcolor('light green')
 
-def petals():
+    petals(sunflower)
+    flowercenter(sunflower)
+    make_text(sunflower)
+
+    s.exitonclick()
+
+
+def flowercenter(sunflower):
+    """
+    Create the center bulb to our flower!
+    """
+    sunflower.hideturtle()
+    sunflower.speed(0)
+    sunflower.goto(-15, -65)
+    sunflower.color("#503D02")
+    sunflower.begin_fill()
+    sunflower.circle(50)  # draws a circle of radius 20
+    sunflower.end_fill()
+
+
+def petals(sunflower):
     """
     Creates the petals to our sunflower!
     """
-sunflower.penup()
-sunflower.goto(0,20)
-sunflower.pendown()
-for i in range(17):
+    sunflower.penup()
+    sunflower.goto(0, 20)
+    sunflower.pendown()
+    for i in range(17):
         sunflower.color('gold')
         sunflower.begin_fill()
         sunflower.circle(190, 90)
@@ -49,17 +58,16 @@ for i in range(17):
         sunflower.left(18)
         sunflower.end_fill()
 
-def make_text():
+
+def make_text(sunflower):
     """
     Writes text about fav flower on the screen.
     """
-sunflower.penup()
-sunflower.goto(0,-200)
-sunflower.pendown()
-sunflower.color("#086054")
-sunflower.write("Sunflowers are my favourite!",move=False,align='center',font=("Arial",30,("bold","normal")))
+    sunflower.penup()
+    sunflower.goto(0, -200)
+    sunflower.pendown()
+    sunflower.color("#086054")
+    sunflower.write("Sunflowers are my favourite!", move=False, align='center', font=("Arial", 30, ("bold", "normal")))
+
 
 main()  # Starts the program!
-s.exitonclick()
-
-
