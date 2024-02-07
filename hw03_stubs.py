30b3e37ec227474f9ea09278d52b1353f7a519e0
bahenar <bahenar@berea.edu>
2024-02-06 19:59:04 -0500

    Rica's building with a tree on the left

--- hw03_stubs.py
+++ hw03_stubs.py
@@ -3,12 +3,12 @@
 #        hw03_username.py and copy this code into it!
 
 #################################################################################
-# Author:
-# Username:
+# Author: Ricardo Bahena
+# Username: Rica
 #
-# Assignment:
-# Purpose:
-# Google Doc Link:
+# Assignment: homework 3
+# Purpose: Buildng a house with a tree on the left side
+# Google Doc Link: https://docs.google.com/document/d/1yAKz379Sjhg7QhFZoLcn1pLcg9lehsnHS3vDrVSa9Vo/edit?usp=sharing
 #
 #################################################################################
 # Acknowledgements:
@@ -19,20 +19,90 @@
 import turtle
 
 
-def function_1():
+def house(dog):
     """
-    Example docstring for function_1. function_1 is not a good function name and should be changed.
+    this is the function house and it's cretaing a house with one window in the tope left
     """
-    pass
-    # ....
+    dog.left(90)
+    dog.forward(250)
+    dog.right(90)
+    dog.forward(150)
+    dog.right(90)
+    dog.forward(250)
+    dog.right(90)
+    dog.forward(150)
+    dog.penup()
+    dog.right(90)
+    dog.forward(150)
+    dog.pendown()
+    dog.forward(50)
+    dog.right(90)
+    dog.forward(50)
+    dog.right(90)
+    dog.forward(50)
+    dog.right(90)
+    dog.forward(50)
+    dog.right(90)
+    dog.penup()
+    dog.forward(25)
+    dog.pendown()
+    dog.right(90)
+    dog.forward(50)
+    dog.right(90)
+    dog.forward(25)
+    dog.right(90)
+    dog.forward(25)
+    dog.right(90)
+    dog.forward(50)
+
+def door(dog):
+    """
+    this is the fucntion of the door where it's being created in the bottom right
+    """
+    dog.penup()
+    dog.goto(0,0)
+    dog.right(90)
+    dog.forward(100)
+    dog.pendown()
+    dog.left(90)
+    dog.forward(40)
+    dog.right(90)
+    dog.forward(50)
+    dog.right(90)
+    dog.forward(40)
+    dog.right(90)
+    dog.forward(25)
+    dog.right(90)
+    dog.forward(40)
 
 
-def function_2():
+
+
+
+
+def tree(dog):
     """
-    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    this is the function where a tree is being created on the left side of the house
     """
-    pass
-    # ...
+    dog.penup()
+    dog.goto(-150,0)
+    dog.pendown()
+    dog.forward(40)
+    dog.penup()
+    dog.goto(-180,0)
+    dog.pendown()
+    dog.forward(40)
+    dog.right(45)
+    dog.forward(23)
+    dog.right(90)
+    dog.forward(25)
+    dog.right(135)
+    dog.forward(40)
+    dog.penup()
+    dog.goto(-150, 0)
+    dog.pendown()
+    dog.forward(40)
+
 
 
 def main():
@@ -41,8 +111,14 @@ def main():
     """
 
     # Function calls to function_1 and function_2.
-    function_1()            # TODO  Remove when you replace it with your function
-    function_2()            # TODO  Remove when you replace it with your function
+    screen= turtle.Screen()
+    screen.bgcolor("lightblue")
+    dog = turtle.Turtle()
+
+    house(dog)
+    door(dog)
+    tree(dog)
+    screen.exitonclick()
 
 
 main()  # Starts the program!
