956fb7505f3fc5149930d44e6e0479045fc95357
murphya <murphya@berea.edu>
2024-02-07 09:30:11 -0500

    worked on colors and larger objects

--- hw03_murphya.py
+++ hw03_murphya.py
@@ -1,8 +1,11 @@
 import turtle
 
-
+wn = turtle.Screen()
+abby = turtle.Screen()
+abby.bgpic('starry.gif')
 
 abby = turtle.Turtle()
+aaliyah = turtle.Turtle()
 
 #This code makes the horizon line and makes the water blue.
 abby.color("#3A3C8E", "#3A3C8E")
@@ -39,8 +42,29 @@ def drawRectangle(t, w, h):
         abby.forward(h)
         abby.left(90)
         abby.end_fill()
+
+#worked on the boat
+aaliyah.penup()
+aaliyah.goto(-90,-160)
+aaliyah.pendown()
+aaliyah.color("#8E5B3A", "#8E5B3A")
+aaliyah.begin_fill()
+aaliyah.right(90)
+aaliyah.circle(100,180)
+aaliyah.left(90)
+aaliyah.forward(200)
+aaliyah.end_fill()
+aaliyah.backward(100)
+aaliyah.right(90)
+aaliyah.forward(100)
+def drawFlag(t, sz):
+
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+
 def main():
-    wn = turtle.Screen()
     drawRectangle(abby, 180, 75)
+    drawFlag(aaliyah, 50)
     wn.exitonclick()
 main()
