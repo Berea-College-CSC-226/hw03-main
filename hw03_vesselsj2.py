2f69ba446235a2d2d4a7b07972944461d9c0cc48
vesselsj2 <vesselsj2@berea.edu>
2024-02-07 07:50:32 -0500

    Created my code

---
+++ hw03_vesselsj2.py
@@ -0,0 +1,104 @@
+#################################################################################
+# Author: Jaren Vessels
+# Username: vesselsj2
+#
+# Assignment: T03: Boustrophedon Turtles
+# Purpose: To draw an illusion with a person floating on it
+#################################################################################
+# Acknowledgements:
+#   Runestone Academy Chapter 5: Modules
+#   Used AI to help debug random module that was not letting me randomize the color of my screen and turtles
+#   Used AI to help me debug my illusion pattern that was not increasing in size on each repetition
+#################################################################################
+
+import turtle
+import random
+
+
+def ran_col():  # Creating the random rgb generator for my code
+    r = int(random.random() * 255)
+    g = int(random.random() * 255)
+    b = int(random.random() * 255)
+    return r, g, b
+
+
+def draw_person(ben, wn):
+    wn.bgcolor(ran_col())  # Randomizes the color of the background screen when this function is called
+    ben.speed(0)
+    ben.pensize(10)
+    ben.penup()
+    ben.goto(-100, 50)  # Setting up the position of ben to draw the person
+    ben.right(90)
+    ben.pendown()
+
+    ben.color(ran_col())  # Randomizes the color of the person's head
+
+    for i in range(4):  # This is drawing the person's head
+        ben.forward(50)
+        ben.left(90)
+
+    ben.color(ran_col())  # Randomizes the color of the person's torso
+
+    ben.penup()  # This is drawing the person's torso
+    ben.goto(-50, 25)
+    ben.left(90)
+    ben.pendown()
+    ben.forward(130)
+
+    ben.color(ran_col())  # Randomizes the color of the person's legs
+
+    ben.left(45)  # This is drawing the person's legs
+    ben.forward(80)
+    ben.penup()
+    ben.goto(80, 25)
+    ben.right(90)
+    ben.pendown()
+    ben.forward(80)
+    ben.penup()
+
+    ben.color(ran_col())  # Randomizes the color of the person's arms
+
+    ben.goto(10, 25)  # This is drawing the person's arms
+    ben.pendown()
+    ben.right(90)
+    ben.forward(80)
+    ben.right(90)
+    ben.forward(35)
+    ben.penup()
+    ben.goto(10, 25)
+    ben.pendown()
+    ben.forward(80)
+    ben.left(90)
+    ben.forward(35)
+
+
+pass
+
+
+def draw_illusion(gilbert, wn):
+    wn.bgcolor(ran_col())  # Randomizes the color of the background screen when this function is called
+    gilbert.speed(0)
+    gilbert.color(ran_col())  # Randomizes the color of the illusion
+    gilbert.pensize(2)
+    length = 2  # Defines the length of each line the turtle makes, so it can increase
+    for i in range(400):  # Draws the entire illusion
+        gilbert.forward(length)
+        gilbert.left(270)
+        length += 2  # Increases the line of the turtle by 2 each time
+
+    gilbert.hideturtle()
+
+    pass
+
+
+def main():
+    wn = turtle.Screen()
+    wn.colormode(255)
+    gilbert = turtle.Turtle()
+    draw_illusion(gilbert, wn)
+    ben = turtle.Turtle()
+    draw_person(ben, wn)
+    wn.exitonclick()
+
+
+main()
