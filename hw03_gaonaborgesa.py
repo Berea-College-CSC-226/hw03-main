869d55594ac853a37b62b597cda894cc5878abdd
gaonaborgesa <gaonaborgesa@berea.edu>
2024-02-07 17:48:49 -0500

    fixed comments

--- hw03_gaonaborgesa.py
+++ hw03_gaonaborgesa.py
@@ -26,11 +26,11 @@ def draw_grass(t):
     :param t: turtle object
     :return: None
     """
-    t.penup()              #positions turtle
-    t.setpos(-310,-200)
+    t.penup()              # positions turtle
+    t.setpos(-310, -200)
     t.pendown()
 
-    for i in range(2):          #makes grass
+    for i in range(2):          # makes grass
         t.forward(610)
         t.right(90)
         t.forward(25)
@@ -48,39 +48,38 @@ def draw_sun(t):
     :param t: turtle object
     :return: None
     """
-    t.penup()              #positions turtle
+    t.penup()              # positions turtle
     t.setpos(212, 265)
     t.pendown()
     t.right(105)
 
-    for i in range(17):       #sun
+    for i in range(17):       # sun
         t.forward(10)
         t.left(10)
 
-    t.penup()              #sun rays 1/4
+    t.penup()              # sun rays 1/4
     t.setpos(187, 250)
     t.pendown()
     t.left(100)
     t.forward(25)
     t.penup()
 
-    t.setpos(195, 210)      #sun rays 2/4
+    t.setpos(195, 210)      # sun rays 2/4
     t.pendown()
     t.left(45)
     t.forward(25)
     t.penup()
 
-    t.setpos(230, 178)      #sun rays 3/4
+    t.setpos(230, 178)      # sun rays 3/4
     t.pendown()
     t.left(35)
     t.forward(25)
     t.penup()
 
-    t.setpos(280, 165)      #sun rays 4/4
+    t.setpos(280, 165)      # sun rays 4/4
     t.pendown()
     t.left(25)
     t.forward(25)
-
 
 
 def draw_flower(t):
@@ -91,19 +90,18 @@ def draw_flower(t):
     :return: None
     """
 
-    stem = turtle.Turtle()        #flower stem
+    stem = turtle.Turtle()        # flower stem
     stem.color("green")
     stem.pensize(15)
-    stem.setpos(0,-200)
+    stem.setpos(0, -200)
     stem.left(90)
     stem.forward(200)
     stem.speed(10)
 
-
     t.begin_fill()
-    t.color("#dc99ec")    #DC99EC (light purple)
+    t.color("#dc99ec")    # DC99EC (light purple)
 
-    for i in range(8):         #flower petals
+    for i in range(8):         # flower petals
         t.right(20)
         t.forward(70)
         t.left(30)
@@ -127,28 +125,28 @@ def main():
 
     wn.bgcolor("lightblue")
 
-    peter = turtle.Turtle()         #turtle for grass
+    peter = turtle.Turtle()         # turtle for grass
     peter.pensize(25)
     peter.color("green")
     peter.hideturtle()
     peter.speed(0)
 
-    lois = turtle.Turtle()          #turtle for flower
+    lois = turtle.Turtle()          # turtle for flower
     lois.pensize(10)
     lois.hideturtle()
     lois.speed(7)
 
-    meg = turtle.Turtle()            #turtle for sun
+    meg = turtle.Turtle()            # turtle for sun
     meg.pensize(10)
     meg.pencolor("yellow")
     meg.hideturtle()
     meg.speed(9)
-
 
     draw_grass(peter)
     draw_sun(meg)
     draw_flower(lois)
 
     wn.exitonclick()
+
 
 main()
