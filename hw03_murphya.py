bc8c5f381edf338312ace3b605760d2d4b0529d7
murphya <murphya@berea.edu>
2024-02-07 23:52:36 -0500

    fixed some stuff!!

--- hw03_murphya.py
+++ hw03_murphya.py
@@ -1,72 +1,31 @@
-# Author: Aaliyah Murphy
-# Username: Murphya
+#################################################################################
+# Author:Aaliyah Murphy
+# Username:Murphya
 #
-# Assignment: Hw03
+# Assignment:Hw03
+# Purpose:Turtle practice and GIT branching.
 # Google Doc Link:https://docs.google.com/document/d/1QOq5mSUdxNJacGKRdWqrQEk5mgNh5cuqwZO52XxC4JY/edit?usp=sharing
 #
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
 
 import turtle
 
-wn = turtle.Screen()
-abby = turtle.Screen()
-abby.bgpic('skyy.gif')
-
-abby = turtle.Turtle()
-aaliyah = turtle.Turtle()
-
-#This code makes the horizon line and makes the water blue.
-abby.color("#5980C3", "#5980C3")
-abby.begin_fill()
-abby.penup()
-abby.right(90)
-abby.forward(65)
-abby.pendown()
-abby.left(90)
-abby.forward(375)
-abby.left(180)
-abby.forward(700)
-abby.left(90)
-abby.forward(200)
-abby.left(90)
-abby.forward(700)
-abby.left(90)
-abby.forward(200)
-abby.left(90)
-abby.end_fill()
-
-abby.penup()
-abby.goto(-40, -65)
-abby.pendown()
-#This code makes the ship dock
-abby.forward(250)
-abby.left(90)
-abby.color("#8E5B3A", "#8E5B3A")
 def drawRectangle(t, w, h):
+    """Makes rectangle with sides 180 and 75"""
     for i in range(2):
-        abby.begin_fill()
-        abby.forward(w)
-        abby.left(90)
-        abby.forward(h)
-        abby.left(90)
-        abby.end_fill()
-
-#worked on the boat
-aaliyah.penup()
-aaliyah.goto(-90,-150)
-aaliyah.pendown()
-aaliyah.color("#8E5B3A", "#8E5B3A")
-aaliyah.begin_fill()
-aaliyah.right(90)
-aaliyah.circle(100,180)
-aaliyah.left(90)
-aaliyah.forward(200)
-aaliyah.end_fill()
-aaliyah.backward(100)
-aaliyah.right(90)
-aaliyah.forward(75)
-aaliyah.backward(15)
+        t.begin_fill()
+        t.forward(w)
+        t.left(90)
+        t.forward(h)
+        t.left(90)
+        t.end_fill()
 
 def drawFlag(t, sz):
+    """Makes square with sides 50"""
     t.begin_fill()
     for i in range(4):
         t.color("#A24C66", "#A24C66")
@@ -74,7 +33,58 @@ def drawFlag(t, sz):
         t.left(90)
     t.end_fill()
 
-#fishing pole
+def drawBoat(t):
+    """Draws a boat with the given turtle t"""
+    t.penup()
+    t.goto(-90,-150)
+    t.pendown()
+    t.color("#8E5B3A", "#8E5B3A")
+    t.begin_fill()
+    t.right(90)
+    t.circle(100,180)
+    t.left(90)
+    t.forward(200)
+    t.end_fill()
+    t.backward(100)
+    t.right(90)
+    t.forward(75)
+    t.backward(15)
+
+def main():
+    wn = turtle.Screen()
+    wn.bgpic('skyy.gif')
+
+    abby = turtle.Turtle()
+    abby.color("#5980C3", "#5980C3")
+    abby.begin_fill()
+    abby.penup()
+    abby.right(90)
+    abby.forward(65)
+    abby.pendown()
+    abby.left(90)
+    abby.forward(375)
+    abby.left(180)
+    abby.forward(700)
+    abby.left(90)
+    abby.forward(200)
+    abby.left(90)
+    abby.forward(700)
+    abby.left(90)
+    abby.forward(200)
+    abby.left(90)
+    abby.end_fill()
+    abby.penup()
+    abby.goto(-40, -65)
+    abby.pendown()
+    abby.forward(250)
+    abby.left(90)
+    abby.color("#8E5B3A", "#8E5B3A")
+    drawRectangle(abby, 180, 75)
+
+    aaliyah = turtle.Turtle()
+    drawBoat(aaliyah)
+
+    drawFlag(aaliyah, 50)
     aaliyah.backward(60)
     aaliyah.right(90)
     aaliyah.forward(50)
@@ -83,8 +93,8 @@ def drawFlag(t, sz):
     aaliyah.right(145)
     aaliyah.forward(140)
     aaliyah.stamp()
-def main():
-    drawRectangle(abby, 180, 75)
-    drawFlag(aaliyah, 50)
+
     wn.exitonclick()
-main()
+
+if __name__ == "__main__":
+    main()
