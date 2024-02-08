4c2094ce7563ce075c436f94ce8148112266acce
morganb2-hw00 <morganb2@berea.edu>
2024-02-07 21:03:38 -0500

    Making changes

--- morganb-hw03.py
+++ morganb-hw03.py
@@ -3,7 +3,7 @@
 # Username:morganb
 #
 # Assignment:Fully Functional Gitty Psychedelic Robotic Turtles
-# Purpose:Continue practicing fucntions and using the turtle library.
+# Purpose:Continue practicing functions and using the turtle library.
 # Google Doc Link:https://docs.google.com/document/d/1NjbTxTAXg0Cuo1YH1nipYNSgiDWNQ2J3X0UAzkHL87w/edit?usp=sharing
 #
 #################################################################################
@@ -15,75 +15,70 @@
 import turtle
 
 
-
-
-
-
-
-
-
-
-def edoras(turtle):
+def edoras(middlearth):
     """
-    Example docstring for function_1. function_1 is not a good function name and should be changed.
+        This is the basic outline of the building.
     """
-    turtle.left(30)
-    turtle.forward(30)
-    turtle.left(60)
-    turtle.forward(60)
-    turtle.right(90)
-    turtle.forward(70)
-    turtle.right(90)
-    turtle.forward(60)
-    turtle.right(90)
-    turtle.forward(70)
-    turtle.left(30)
-    turtle.forward(40)
-    turtle.right(120)
-    turtle.forward(80)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.left(30)
-    turtle.forward(150)
-    turtle.right(120)
-    turtle.forward(120)
-    turtle.right(40)
-    turtle.goto(95.98, -145)
-    turtle.right(140)
-    turtle.forward(60)
-    turtle.left(90)
-    turtle.forward(300)
-    turtle.left(70)
-    turtle.forward(70)
-    turtle.goto(0, -160)
-    turtle.right(160)
-    turtle.forward(230)
-    turtle.right(90)
-    turtle.forward(220)
-    turtle.right(90)
-    turtle.forward(90)
-    turtle.left(180)
-    turtle.forward(90)
-    turtle.left(90)
-    turtle.forward(425)
-    turtle.left(90)
-    turtle.forward(150)
-    turtle.right(180)
-    turtle.forward(150)
-    turtle.left(130)
-    turtle.forward(30)
-    turtle.right(180)
-    turtle.forward(180)
-    turtle.right(87)
-    turtle.forward(150)
-    turtle.left(180)
-    turtle.forward(150)
-    turtle.right(150)
-    turtle.forward(300)
-    # ....
+    middlearth.left(30)
+    middlearth.forward(30)
+    middlearth.left(60)
+    middlearth.forward(60)
+    middlearth.right(90)
+    middlearth.forward(70)
+    middlearth.right(90)
+    middlearth.forward(60)
+    middlearth.right(90)
+    middlearth.forward(70)
+    middlearth.left(30)
+    middlearth.forward(40)
+    middlearth.right(120)
+    middlearth.forward(80)
+    middlearth.right(90)
+    middlearth.forward(100)
+    middlearth.left(30)
+    middlearth.forward(150)
+    middlearth.right(120)
+    middlearth.forward(120)
+    middlearth.right(40)
+    middlearth.goto(95.98, -145)
+    middlearth.right(140)
+    middlearth.forward(60)
+    middlearth.left(90)
+    middlearth.forward(300)
+    middlearth.left(70)
+    middlearth.forward(70)
+    middlearth.goto(0, -160)
+    middlearth.right(160)
+    middlearth.forward(230)
+    middlearth.right(90)
+    middlearth.forward(220)
+    middlearth.right(90)
+    middlearth.forward(90)
+    middlearth.left(180)
+    middlearth.forward(90)
+    middlearth.left(90)
+    middlearth.forward(425)
+    middlearth.left(90)
+    middlearth.forward(150)
+    middlearth.right(180)
+    middlearth.forward(150)
+    middlearth.left(130)
+    middlearth.forward(30)
+    middlearth.right(180)
+    middlearth.forward(180)
+    middlearth.right(87)
+    middlearth.forward(150)
+    middlearth.left(180)
+    middlearth.forward(150)
+    middlearth.right(150)
+    middlearth.forward(300)
+
 
 
 def arches(t):
+    """
+           This is the function to create the arches for the front of the hall.
+       """
     t.penup()
     t.goto(-40,-85)
     t.left(105)
@@ -97,7 +92,7 @@ def arches(t):
         t.right(90)
 
     """
-    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    
     """
     pass
     # ...
@@ -105,7 +100,7 @@ def arches(t):
 
 def main():
     """
-    Docstring for main. Should describe the main functionality of this file.
+
     """
     rohan = turtle.Screen()
     turtle.turtles()
@@ -119,10 +114,10 @@ def main():
     theoden.setpos(0, -160)
     theoden.pendown()
     # Function calls to function_1 and function_2.
-    edoras(theoden)            # TODO  Remove when you replace it with your function
-    arches(theoden)            # TODO  Remove when you replace it with your function
+    edoras(theoden)
+    arches(theoden)
 
     rohan.exitonclick()
 
-main()  # Starts the program!
+main()
 
