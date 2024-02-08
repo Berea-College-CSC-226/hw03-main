85496b56a6b55b199b5ab6d25d3b9606c1aec141
smithj7 <smithj7@berea.edu>
2024-02-08 00:11:58 -0500

    I made the changes to the code again- pt.3 commit (Hw03)

--- hw03_smithj7.py
+++ hw03_smithj7.py
@@ -12,41 +12,34 @@
 ###############################
 
 import turtle
-
 def drawsquare(JK):     ##Draws House
-    """
-    Drawing the Square Turtle
-    """
-    JK.color('red')
-    JK.pensize(20)
-    JK.penup()
-    JK.goto(-300,-300)
-    JK.pendown()
+  """Drawing the Square Turtle"""
 
-    for i in range(4):
-        JK.forward(250)
-        JK.left(90)
+  JK.color('red')
+  JK.pensize(20)
+  JK.penup()
+  JK.goto(-300,-300)
+  JK.pendown()
+
+  for i in range(4):
+    JK.forward(250)
+    JK.left(90)
 
 def drawTriangle(JK): ## this is the roof
-    """
-        Drawing the roof Turtle
-        """
-    ##Draws roof
-    JK.pencolor(255,199,44)
-    JK.pensize(25)
-    JK.penup()
-    JK.goto(-335,-55)
-    JK.pendown()
+ """Drawing the roof Turtle"""
+ JK.pencolor(255,199,44)
+ JK.pensize(25)
+ JK.penup()
+ JK.goto(-335,-55)
+ JK.pendown()
 
-    for i in range(2):
-        JK.right(-45)
-        JK.forward(230)
-        JK.right(135)
+ for i in range(2):
+      JK.right(-45)
+      JK.forward(230)
+      JK.right(135)
 
 def drawsign(JK):       ##Draws "M" in MC donalads sign
-    """
-        Drawing the M in McDonalds sign
-        """
+    """Drawing the M in McDonalds sign"""
     JK.penup()
     JK.pensize(10)
     JK.goto(-220,-220)
@@ -64,7 +57,6 @@ def drawsign(JK):       ##Draws "M" in MC donalads sign
         JK.right(90)
 
 def drawletterC(JK):        ##Draws "c" in MC sign
-
     """ Drawing the c in McDonalds sign """
     JK.penup()
     JK.goto(-150,-220)
@@ -85,10 +77,10 @@ def main(): #sets up main
     JK.hideturtle()
     JK.speed(4)
 
-    drawsquare(JK)  # call to function_1    House
-    drawTriangle(JK)  # call to function_2
-    drawsign(JK) #call Function 3 letter M
+    drawsquare(JK)     # call to function_1    House
+    drawTriangle(JK)    # call to function_2
+    drawsign(JK)      #call Function 3 letter M
     drawletterC(JK)      #draw function 4 letter c
     wn.exitonclick()
 
-main()      ##Invoke Main
+main()        ##Invoke Main
