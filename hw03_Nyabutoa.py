793e664d4760254d941022ffd133362fbb56ad61
arphaxadnyabuto <Nyabutoa@Berea.edu>
2024-09-17 16:17:05 -0400

    did some fixing and still not completed with strings. will finish after work.

--- hw03_Nyabutoa.py
+++ hw03_Nyabutoa.py
@@ -17,9 +17,16 @@
 import turtle
 
 
-def circlec(nyabutoa):
-    nyabutoa.goto(0,0)
+def draw_circle(nyabutoa):
+
+#we will be drawing the head using a circle, which serves as a foundational shape for the illustration.
+
+    nyabutoa.goto(0,0)    # The command "nyabutoa.goto(0, 0)" sets the starting point at the center of the circle.The circle will be drawn outwards.
     r=180
+
+# In this function, 'r' stands for radius, which determines the size of the circle.
+ # The larger the radius, the bigger the circle will be; this directly affects the size of the head being drawn.
+
     nyabutoa.pensize(10)
     nyabutoa.circle(r)
     nyabutoa.left(90)
@@ -28,7 +35,7 @@ def circlec(nyabutoa):
     nyabutoa.left(90)
     nyabutoa.forward(100)
     nyabutoa.pendown()
-    eyez(nyabutoa)
+    Draw_eyes(nyabutoa)
     nyabutoa.right(90)
     nyabutoa.penup()
     nyabutoa.forward(20)
@@ -36,12 +43,16 @@ def circlec(nyabutoa):
     nyabutoa.forward(200)
     nyabutoa.left(180)
     nyabutoa.pendown()
-    eyez(nyabutoa)
+    Draw_eyes(nyabutoa)
 
 
 
-def eyez(eye1):
+def Draw_eyes(eye1):
     e_r=30
+
+# The variable 'e_r' stands for eye radius.
+# It defines the size of the eyes to be drawn in relation to the other elements of the drawing.
+
     eye1.pensize(4)
     for i in range(5):
         eye1.circle(e_r)
@@ -59,7 +70,7 @@ def main():
     Nyabutoa=turtle.Turtle()
     wn = turtle.Screen()
     wn.bgcolor("Purple")
-    circlec(Nyabutoa)
+    draw_circle(Nyabutoa)
    # eyez(Nyabutoa)
     wn.exitonclick()
 
