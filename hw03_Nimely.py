926d6236e01c100a1dc7d6a0d1365026390a1e12
nimelyj <nimelyj@berea.edu>
2024-09-17 01:03:37 -0400

    Drew the house and a flower by it using functions.

--- hw03_Nimely.py
+++ hw03_Nimely.py
@@ -1,6 +1,6 @@
 #################################################################################
-# Author: Joyce Nimely
-# Username:Nimely
+# Author: flower stem
+# Username:stem
 #
 # Assignment:Fully Functional Gitty Psychedelic Robotic Turtles
 # Purpose:
@@ -16,71 +16,80 @@
 import turtle
 
 #define function to draw background
-def  draw_background():
-    turtle.bgcolor("sky blue")
+def  draw_background(screen: turtle.TurtleScreen ):
+    screen.colormode(255)
+    screen.bgcolor(127,103,103)
 
-def draw_house():
-    turtle.penup()
-    turtle.pensize(4)
-    turtle.goto(-50,-50) #move turtle to a starting position
-    turtle.pendown()
-    turtle.color('brown')
-    turtle.begin_fill()
+
+def draw_house(house: turtle.Turtle):
+    house.penup()
+    house.pensize(4)
+    house.goto(-50,-50) #move turtle to a starting position
+    house.pendown()
+
+
+    house.color(136, 166, 99)
+    house.begin_fill()
     for i in range(4):
-        turtle.forward(100)
-        turtle.left(90)
-        turtle.end_fill()
+        house.forward(100)
+        house.left(90)
+        house.end_fill()
 
     # Draw the roof on top of  the house
-    turtle.penup()
-    turtle.pensize(5)
-    turtle.goto(-50,45)
-    turtle.pendown()
-    turtle.bgcolor('sky blue')
-    turtle.color('red')
-    turtle.begin_fill()
-    turtle.left(45)
-    turtle.forward(70)
-    turtle.right(90)
-    turtle.forward(70)
-    turtle.left(135)
-    turtle.forward(100)
+    house.penup()
+    house.pensize(5)
+    house.goto(-50,45)
+    house.pendown()
+    house.color(27, 14, 14)
+    house.begin_fill()
+    house.left(45)
+    house.forward(70)
+    house.right(90)
+    house.forward(70)
+    house.left(135)
+    house.forward(100)
 
-def draw_flower():
+def draw_flower(flower:turtle.Turtle, stem: turtle.Turtle):
     """Draws a flower on the side of the house."""
-    joyce = turtle.Turtle()
-    nimely = turtle.Turtle()
 
-    joyce.color('yellow')
-    joyce.speed(25)
-    joyce.pensize(3)
-    nimely.color('green')
-    nimely.speed(15)
-    nimely.pensize(10)
+    flower.color(27, 27, 14)
+    flower.speed(25)
+    flower.pensize(3)
+    stem.color(16,27,14)
+    stem.speed(15)
+    stem.pensize(10)
 
     # Draw flower petals
-    joyce.penup()
-    joyce.goto(-150, -50)  # Adjusted position to the side of the house
-    joyce.pendown()
+    flower.penup()
+    flower.goto(-150, -50)  # Adjusted position to the side of the house
+    flower.pendown()
     for _ in range(50):
-        joyce.circle(50, 60)
-        joyce.left(120)
-        joyce.circle(50, 60)
-        joyce.left(160)
-        joyce.right(50)
+        flower.circle(50, 60)
+        flower.left(120)
+        flower.circle(50, 60)
+        flower.left(160)
+        flower.right(50)
 
     # Draw the stem
-    nimely.penup()
-    nimely.goto(-150, -100)  # Adjusted position for the stem
-    nimely.pendown()
-    nimely.right(90)
-    nimely.forward(100)
+    stem.penup()
+    stem.goto(-150, -100)  # Adjusted position for the stem
+    stem.pendown()
+    stem.right(90)
+    stem.forward(100)
 
 def main():
+    wn = turtle.Turtle()
+    screen = wn.screen
     turtle.speed('fastest')
-    draw_background()
-    draw_house()
-    draw_flower()
+    draw_background(screen)
+
+    house = turtle.Turtle()
+    draw_house(house)
+
+    flower = turtle.Turtle()
+    stem = turtle.Turtle()
+    draw_flower(flower, stem)
+
 
 main()
 
