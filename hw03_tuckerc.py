2ca7c0e41acba932b1a2877996331f413e176480
tuckercberea <tuckerc@berea.edu>
2024-09-18 11:07:22 -0400

    Completed the fixes requested in feedback - created t as the turtle object instead of calling the module, created a screen and changed the exit call to said screen instead of the module

--- hw03_tuckerc.py
+++ hw03_tuckerc.py
@@ -16,62 +16,62 @@
 import turtle
 
 
-def draw_bg():
+def draw_bg(t):
     """
     Creates the required colored background.
     """
-    turtle.fillcolor('#00BFFF') # Blue
-    turtle.penup() # To prevent random lines
-    turtle.goto (-100, -100) # Goes to bottom left corner of future square
-    turtle.begin_fill() # Begins filling the square
-    turtle.left(90)
-    turtle.forward(200)
-    turtle.right(90)
-    turtle.forward(200)
-    turtle.right(90)
-    turtle.forward(200)
-    turtle.right(90)
-    turtle.forward(200)
-    turtle.end_fill() # Ends filling the square
-    turtle.right(180) # Faces to original position
+    t.fillcolor('#00BFFF') # Blue
+    t.penup() # To prevent random lines
+    t.goto (-100, -100) # Goes to bottom left corner of future square
+    t.begin_fill() # Begins filling the square
+    t.left(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.end_fill() # Ends filling the square
+    t.right(180) # Faces to original position
 
 
-def draw_duck():
+def draw_duck(t):
     """
     Draws the duck.
     """
     # Draws the duck's body
-    turtle.goto(0, -50)
-    turtle.color('#FFFF00') # Yellow
-    turtle.begin_fill()
-    turtle.circle(50)
-    turtle.end_fill()
+    t.goto(0, -50)
+    t.color('#FFFF00') # Yellow
+    t.begin_fill()
+    t.circle(50)
+    t.end_fill()
 
     # Draws the duck's bill
-    turtle.goto(50, 30)
-    turtle.color('#FFA500')  # Orange
-    turtle.begin_fill()
-    turtle.goto(50, 38)
-    turtle.goto(70, 30)
-    turtle.goto(50, 26)
-    turtle.goto(50, 38)
-    turtle.end_fill()
+    t.goto(50, 30)
+    t.color('#FFA500')  # Orange
+    t.begin_fill()
+    t.goto(50, 38)
+    t.goto(70, 30)
+    t.goto(50, 26)
+    t.goto(50, 38)
+    t.end_fill()
 
     # Draws the duck's head
-    turtle.goto(40, 20)
-    turtle.color('#FFFF00')  # Yellow
-    turtle.begin_fill()
-    turtle.circle(20)
-    turtle.end_fill()
+    t.goto(40, 20)
+    t.color('#FFFF00')  # Yellow
+    t.begin_fill()
+    t.circle(20)
+    t.end_fill()
 
     # Draws the duck's beady black eyeball
-    turtle.goto(50, 40)
-    turtle.color('#000000')  # Black
-    turtle.begin_fill()
-    turtle.circle(5)
-    turtle.end_fill()
+    t.goto(50, 40)
+    t.color('#000000')  # Black
+    t.begin_fill()
+    t.circle(5)
+    t.end_fill()
 
-    turtle.hideturtle() # Hides the turtle because it looks ugly at the end
+    t.hideturtle() # Hides the turtle because it looks ugly at the end
 
 
 
@@ -79,9 +79,11 @@ def main():
     """
     The main function that calls the other two functions, which draw the background and then the duck.
     """
-    draw_bg()
-    draw_duck()
-    turtle.exitonclick() # Waits until the user clicks to exit the drawing so you can admire the pretty duck
+    wn = turtle.Screen()
+    t = turtle.Turtle()
+    draw_bg(t)
+    draw_duck(t)
+    wn.exitonclick() # Waits until the user clicks to exit the drawing so you can admire the pretty duck
 
 
 main()  # The beginning of something great
