f0936b72c959c6b53848c6ac19a92e5e7e7d0525
davism5 <davism5@berea.edu>
2024-09-17 21:10:37 -0400

    I have a house, tree, and background and have only functions and main, but have to finish the questions, door and window.

--- hw03_davism5.py
+++ hw03_davism5.py
@@ -12,31 +12,114 @@ import turtle
 
 def house(t):
     for i in range(4):
-        t.forward(200)
+        t.forward(300)
         t.left(90)
     return t
 
 def roof(t):
-    for i in range(3):
-         t.forward(100)
-         t.left(120)
+    for i in range(1):
+         t.left(140)
+         t.forward(250)
+         t.left(77)
+         t.forward(260)
+         t.left(143)
+         t.forward(390)
+
+    return t
+
+
+def tree(t):
+    for i in range(2):
+        t.forward(100)
+        t.left(90)
+        t.forward(200)
+        t.left(90)
+
+    return t
+
+def window_door(t):
+    for i in range(2):
+        t.forward(120)
+        t.left(90)
+
+
     return t
 
 def create_turtle(pensize, color):
     t_turtle=turtle.Turtle()
     t_turtle.pensize(pensize)
     t_turtle.color(color)
-    return t_turtle
 
+    return t_turtle
 
 def main():
     wn=turtle.Screen()
+    wn.bgcolor('lightblue')
     wn.screensize(canvwidth=600, canvheight=600)
 
-    t_house= create_turtle(pensize=10, color="red")
+    t_house= create_turtle(pensize=10, color="hotpink")
     t_house.penup()
-    t_house.setposition(-250, -250)
+    t_house.setposition(-280, -280)
     t_house.pendown()
+
+    t_house.color('hotpink')
+    t_house.begin_fill()
+    for n in range(4):
+        t_house.forward(400)
+        t_house.left(90)
+    t_house.end_fill()
+
+    t_roof= create_turtle(pensize=10, color="purple")
+    t_roof.penup()
+    t_roof.setposition(123,123)
+    t_roof.pendown()
+
+    t_roof.color('purple')
+    t_roof.begin_fill()
+    for n in range(1):
+        t_roof.left(140)
+        t_roof.forward(250)
+        t_roof.left(77)
+        t_roof.forward(260)
+        t_roof.left(143)
+        t_roof.forward(390)
+    t_roof.end_fill()
+
+    t_tree= create_turtle(pensize=5, color="brown")
+    t_tree.penup()
+    t_tree.setposition(280,-280)
+    t_tree.pendown()
+
+    t_tree.color('brown')
+    t_tree.begin_fill()
+
+    for side in range(2):
+        t_tree.forward(100)
+        t_tree.left(90)
+        t_tree.forward(200)
+        t_tree.left(90)
+    t_tree.end_fill()
+
+    t_tree.color('green')
+    t_tree.begin_fill()
+    t_tree.penup()
+    t_tree.setposition(335,-90)
+    t_tree.pendown()
+    t_tree.color("green")
+    t_tree.circle(120)
+    t_tree.end_fill()
+
+    t_window_door = create_turtle(pensize=2, color="brown")
+    t_window_door.penup()
+    t_window_door.setposition(-130,-30)
+    t_window_door.pendown()
+
+
+    t_window_door.color('black')
+    for i in range(4):
+        t_window_door.forward(120)
+        t_window_door.left(90)
+
 
     wn.exitonclick()
 
