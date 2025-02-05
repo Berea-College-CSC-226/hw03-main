bf5ddfbb42374d7b595fb914ce44b9b3291d87ac
eboselea <eboselea@berea.edu>
2025-02-04 20:13:54 -0500

    Finished Sections 1 and 2, writing code for house

--- PDFs/hw03_eboselea.py
+++ PDFs/hw03_eboselea.py
@@ -18,21 +18,61 @@
 
 import turtle
 
+# def make_roof(wn, shape):
+"""
+    Creates the roof with a navy blue color
 
-def function_1():
+    :param wn: a turtle Screen object
+    :param shape: a Turtle object
+    :return: None
     """
-    Example docstring for function_1. function_1 is not a good function name and should be changed.
+    # wn.register_shape()  # Registers a shape so it can be used by the turtle library
+    # shape.penup()
+    # shape.setpos(20, 20)
+    # shape.pendown()
+    # shape.shape("navy blue")  # Sets the shape to the image registered above
+    # shape.stamp()
+
+def make_main_house(shape):
     """
-    pass
+    Creates the base of the house in the shape of a square
+
+    :param shape: aTurtle object
+    :return: None
+    """
+    shape.setpos(-40, -30)
+    shape.color('#2C7A8A')
+    shape.begin_fill()
+    for side in range(2):
+        shape.forward(100)
+        shape.left(90)
+        shape.forward(150)
+        shape.left(90)
+
+    shape.end_fill()
+
+
     # ....
 
 
-def function_2():
+def make_door(shape):
     """
-    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    Add a door for the house
+
+    :param shape: a turtle object
+    :return: None
     """
-    pass
-    # ...
+    shape.penup()
+    shape.setpos(-8, -30)
+    shape.pendown()
+    shape.color('#1C1C25')
+    shape.begin_fill()
+    for side in range(2):
+        shape.forward(40)
+        shape.left(90)
+        shape.forward(65)
+        shape.left(90)
+    shape.end_fill()
 
 
 def main():
@@ -41,13 +81,19 @@ def main():
     """
     wn = turtle.Screen()
     wn.colormode(255)
-    wn.bgpic("Street.gif")
+    wn.bgcolor("light blue")
     shape = turtle.Turtle()
+    shape.hideturtle()
+
+    # make_roof(wn, shape)
+    make_main_house(shape)
+    make_door(shape)
+    wn.exitonclick()
     
     
     # Function calls to function_1 and function_2.
-    function_1()
-    function_2()
+    # function_1()
+    # function_2()
 
 
 main()  # Starts the program!
