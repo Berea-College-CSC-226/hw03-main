bcbbbdfcc6c4f0cebcf14778f6144fb45fd2debb
RobinsonS255 <robinsons3@berea.edu>
2025-09-15 20:04:10 -0400

    Finished coding dog

--- hw03_robinsons3.py
+++ hw03_robinsons3.py
@@ -12,7 +12,7 @@ import turtle
 t = turtle.Turtle()
 t.speed(0)
 wn = t.getscreen()
-
+t.hideturtle()
 
 def semicircle(dir):
     #Makes a semi-circle for use in other functions like leg()
@@ -56,6 +56,81 @@ def main():
     t.left(90)
     t.forward(-23)
     t.forward(23)
+
+    #Left side of body
+    t.right(90)
+    t.forward(45)
+    t.left(90)
+
+    #Head
+    for i in range(72):
+        t.forward(2)
+        t.right(5)
+    for i in range(25):
+        t.forward(2)
+        t.right(5)
+    t.left(45)
+    t.forward(25)
+    t.right(135)
+    t.forward(25)
+    t.left(45)
+    for i in range(5):
+        t.forward(2)
+        t.right(5)
+    t.left(45)
+    t.forward(25)
+    t.right(135)
+    t.forward(27)
+    t.left(45)
+
+    #Back to start
+    t.penup()
+    t.setpos(0, 0)
+    t.left(150)
+
+    #Right Body + Tail
+    t.pendown()
+    t.forward(30)
+    t.right(90)
+    leg(30,"left")
+
+    #Top of body
+    t.forward(98)
+    t.penup()
+
+    #Mouth
+    t.right(45)
+    t.fd(10)
+    t.pendown()
+    for i in range(2):
+        t.left(90)
+        t.fd(10)
+        t.right(90)
+        t.fd(10)
+    t.penup()
+
+    #Eye 1
+    t.right(45)
+    t.forward(5)
+    t.begin_fill()
+    t.pendown()
+    for i in range(6):
+        t.forward(5)
+        t.right(60)
+    t.end_fill()
+    t.penup()
+
+    #Eye 2
+    t.right(90)
+    t.fd(20)
+    t.left(90)
+    t.pendown()
+    t.begin_fill()
+    for i in range(6):
+        t.forward(5)
+        t.right(60)
+    t.end_fill()
+
 
 
     wn.exitonclick()
