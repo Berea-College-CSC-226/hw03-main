7bdd2cdbd5909faa68451c6ceb2da4d5e072e7b9
santosb2 <santosb2@berea.edu>
2025-09-16 12:43:38 -0400

    Almost finished with HW03

--- Answers/hw03_santosb2_flag.py
+++ Answers/hw03_santosb2_flag.py
@@ -13,33 +13,66 @@
 #
 #
 #################################################################################
-
-
 import turtle
+wn = turtle.Screen()
+tess = turtle.Turtle()
 
 
 def make_sqaure():
-    tess = turtle.Turtle()
+    tess.penup()
+    tess.setposition(-200,-100)
+    tess.pendown()
+    tess.begin_fill()
+
     for i in range(2):
-        tess.forward(90)
+        tess.color(0, 156, 59)
+        tess.forward(400)
         tess.left(90)
-        tess.forward(40)
+        tess.forward(270)
         tess.left(90)
+    tess.end_fill()
 
 
-def function_2():
+
+def middle_shape():
+    def make_circle():
+        tess.left(135)
+        tess.begin_fill()
+        tess.penup()
+        tess.setposition(5,-40)
+        tess.pendown()
+        tess.color(7, 7, 99)
+        tess.circle(70)
+        tess.end_fill()
+    def make_weird_square():
+        tess.begin_fill()
+        tess.color("yellow")
+        tess.penup()
+        tess.setposition(-120,30)
+        tess.pendown()
+        tess.right(45)
+        tess.forward(180)
+        tess.left(90)
+        tess.forward(180)
+        tess.left(90)
+        tess.forward(180)
+        tess.left(90)
+        tess.forward(180)
+        tess.end_fill()
+    make_weird_square()
+    make_circle()
 
 
 
 def main():
-    import turtle
-    wn = turtle.Screen()
-    tess = turtle.Turtle()
     wn.colormode(255)
+    wn.bgcolor(152, 163, 175)
+    tess.speed(5)
 
     # Function calls to function_1 and function_2.
     make_sqaure()            #
-    function_2()            #
+    middle_shape()
+    wn.exitonclick()
 
 
 main()  # Starts the program!
