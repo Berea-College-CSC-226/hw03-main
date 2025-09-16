c711ed3d0c5a3bb93593639af08c09df6ddb61a5
santosb2 <santosb2@berea.edu>
2025-09-15 20:07:50 -0400

    Starting the coding part

---
+++ Answers/hw03_santosb2_flag.py
@@ -0,0 +1,45 @@
+
+
+#################################################################################
+# Author:
+# Username:
+#
+# Assignment:
+# Purpose:
+# Google Doc Link:
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+
+import turtle
+
+
+def make_sqaure():
+    tess = turtle.Turtle()
+    for i in range(2):
+        tess.forward(90)
+        tess.left(90)
+        tess.forward(40)
+        tess.left(90)
+
+
+def function_2():
+
+
+
+def main():
+    import turtle
+    wn = turtle.Screen()
+    tess = turtle.Turtle()
+    wn.colormode(255)
+
+    # Function calls to function_1 and function_2.
+    make_sqaure()            #
+    function_2()            #
+
+
+main()  # Starts the program!
