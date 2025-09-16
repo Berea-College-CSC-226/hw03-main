12f0611a2eceb305c2b68d7b9cefe52b3cbc34e7
Ksmun <Ksmun@berea.edu>
2025-09-16 00:21:08 -0400

    Started coding, answered README.md section one and two.

---
+++ hw03_Ksmun.py
@@ -0,0 +1,61 @@
+
+
+#################################################################################
+# Author:Naod Ksmu
+# Username:Ksmun
+#
+# Assignment:https://docs.google.com/document/d/1Iym1JjyM0cK_dIObCt4MmZlPen6iQ6lQ2Xs0fZlrBPs/edit?usp=sharing
+# Purpose:
+# Google Doc Link:https://docs.google.com/document/d/1Iym1JjyM0cK_dIObCt4MmZlPen6iQ6lQ2Xs0fZlrBPs/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+
+
+import turtle
+
+def house_1():
+
+    turtle.pos()
+    turtle.bgcolor("red")
+    turtle.pensize(10)
+    turtle.fd(180)
+    turtle.left(90)
+    turtle.fd(180)
+    turtle.right(90)
+    turtle.fd(-180)
+    turtle.left(-90)
+    turtle.fd(180)
+    turtle.fd(-180)
+    turtle.left(90)
+    turtle.fd(180)
+    turtle.right(105)
+    turtle.fd(180)
+
+
+
+
+
+
+
+
+
+def text_2():
+    """
+    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    """
+    pass
+    # ...
+
+
+def main():
+
+    # Function calls to function_1 and function_2.
+    (house_1())            #
+
+
+main()  # Starts the program!
