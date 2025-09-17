8b997343d213797196854f1b5cf021057bbb16be
santosb2 <santosb2@berea.edu>
2025-09-16 20:20:02 -0400

    Last commit and push for HW03

--- Answers/hw03_santosb2_flag.py
+++ Answers/hw03_santosb2_flag.py
@@ -1,12 +1,12 @@
 
 
 #################################################################################
-# Author:
-# Username:
+# Author:Bernave Santos
+# Username:santosb2
 #
-# Assignment:
+# Assignment:HW03
 # Purpose:
-# Google Doc Link:
+# Google Doc Link: https://docs.google.com/document/d/1dJwVSxuDfwrbqt-lodUltlvsNYP_PXK_SukVk725Yp8/edit?usp=sharing
 #
 #################################################################################
 # Acknowledgements:
@@ -17,13 +17,15 @@ import turtle
 wn = turtle.Screen()
 tess = turtle.Turtle()
 
-
+'''
+So I'll be making the brazil flag since one of my brazilain friends gave me the idea. Also i love brazil 
+and it very pretty so why not. Also isnt to hard to make unlike the mexican flag.
+'''
 def make_sqaure():
     tess.penup()
     tess.setposition(-200,-100)
     tess.pendown()
     tess.begin_fill()
-
     for i in range(2):
         tess.color(0, 156, 59)
         tess.forward(400)
@@ -31,9 +33,15 @@ def make_sqaure():
         tess.forward(270)
         tess.left(90)
     tess.end_fill()
+'''
+So the make_sqaure function helps me create the green flag background and nothing else just wanted the first 
+function to be simple.
+'''
 
-
-
+'''''
+The middle_shape is what it sounds like its the middle shapes in the brazil flag. The diffcult part about this was 
+the make_weird_sqaure, because you just had to make sure the aligned well with the flag.
+'''''
 def middle_shape():
     def make_circle():
         tess.left(135)
@@ -59,9 +67,36 @@ def middle_shape():
         tess.left(90)
         tess.forward(180)
         tess.end_fill()
+    def white_stars():
+        def star():
+            for i in range(5):
+                tess.begin_fill()
+                tess.forward(5)
+                tess.right(144)
+                tess.end_fill()
+        tess.color("white")
+        tess.penup()
+        tess.setposition(0,40)
+        tess.pendown()
+        star()
+        tess.penup()
+        tess.setposition(30,45)
+        tess.pendown()
+        star()
+        tess.penup()
+        tess.setposition(-20, 45)
+        tess.pendown()
+        star()
+
+
     make_weird_square()
     make_circle()
-
+    white_stars()
+"""
+The make_circle function was easy just used a setpostion and circle and fill code to make it. Another function i did 
+was the star function because the brazil flag has a few and keep in mind i did everything i needed to do in this
+assigment so i wasnt going to add a bunch of stars just like three. Again it looks nice having them their. 
+"""
 
 
 def main():
@@ -73,6 +108,8 @@ def main():
     make_sqaure()            #
     middle_shape()
     wn.exitonclick()
-
+'''
+I also used colors from the rbg website 
+'''
 
 main()  # Starts the program!
