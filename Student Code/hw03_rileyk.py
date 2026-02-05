c0292c241dc9c8a4af787049a09cdabc93fea8d1
rileyk <rileyk3@berea.edu>
2026-02-05 17:29:38 -0500

    Answered sections 1-3 and created my python file.

---
+++ Student Code/hw03_rileyk.py
@@ -0,0 +1,31 @@
+#################################################################################
+# Author: Boone Riley
+# Username: rileyk
+#
+# Assignment: HW03: Fully Functional Gitty Psychedelic Robotic Turtles
+# Purpose: Creative drawing of a UFO
+# Google Doc Link: https://docs.google.com/document/d/1Rr4vIPVRLXlNXfkz7AHrMYtzlTRI_a0lpDhTECapNCE/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+import turtle
+
+
+def ufo(alien):
+    alien.pensize(10)
+    alien.pencolor('green')
+    alien.forward(50)
+
+def main():
+    alien = turtle.Turtle()
+    wn = turtle.Screen()
+    wn.bgcolor('purple')
+    ufo(alien)
+    wn.exitonclick()
+
+
+main()
