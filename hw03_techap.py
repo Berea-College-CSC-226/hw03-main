a6ea4b5257f701036f660673d6a14ea5e6bfcf97
Pride-Akana-Techa <techap@berea.edu>
2026-02-07 17:47:12 -0500

    Answered sections 1 through 3.

---
+++ hw03_techap.py
@@ -0,0 +1,45 @@
+#################################################################################
+# Author: Pride Akana Techa
+# Username: techap
+#
+# Assignment: HW03 - Fully Functional Gitty Psychedelic Robotic Turtles
+# Purpose: To show effective use of functions and turtle methods in Python.
+# Google Doc Link: https://docs.google.com/document/d/1RcjcQ88EDyKiG31gs6bNng1g_oic5dEuExgSP8kU784/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+
+import turtle
+
+
+def function_1():
+    """
+    Example docstring for function_1. function_1 is not a good function name and should be changed.
+    """
+    pass
+    # ....
+
+
+def function_2():
+    """
+    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    """
+    pass
+    # ...
+
+
+def main():
+    """
+    Docstring for main. Should describe the main functionality of this file.
+    """
+
+    # Function calls to function_1 and function_2.
+    function_1()            # TODO  Remove when you replace it with your function
+    function_2()            # TODO  Remove when you replace it with your function
+
+
+main()  # Starts the program!
