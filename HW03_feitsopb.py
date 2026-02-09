323b3c58eab15ff865afd33c832bd4aba6a6a6bb
feitsopb <feitsopb@berea.edu>
2026-02-08 20:38:00 -0500

    started working on my code

---
+++ HW03_feitsopb.py
@@ -0,0 +1,18 @@
+#################################################################################
+# Author: Bright Feitsop
+# Username: feitsopb
+#
+# Assignment: house in daylight
+# Purpose: design a beautiful house using turtles
+# Google Doc Link:
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+
+import turtle
+tess = turtle.Turtle()
+
