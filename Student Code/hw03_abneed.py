3da78ac28d00993e14161b4bd639447a725a028a
abneed <abneed@berea.edu>
2026-02-09 09:34:43 -0500

    added copy of code file, answered questions.

---
+++ Student Code/hw03_abneed.py
@@ -0,0 +1,45 @@
+#################################################################################
+# Author: Danny Abnee
+# Username: abneed
+#
+# Assignment: HW03: Fully Functional Gitty Psychedelic Robotic Turtles
+# Purpose: It is all about GIT and not creating merge conflicts
+# Google Doc Link: https://docs.google.com/document/d/1JqFxISAvIxFo8bfUmzAgvcF1u6JWtTEUi2HYTVH7C8U/edit?usp=sharing
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+
+import turtle
+
+
+def function_1():
+    """
+    Example docstring for function_1. function_1 is not a good function name and should be changed.
+    """
+    pass
+    # ....
+
+
+def function_2():
+    """
+    Example docstring for function_2. function_2 is not a good function name and should be changed.
+    """
+    pass
+    # ...
+
+
+def main():
+    """
+    Docstring for main. Should describe the main functionality of this file.
+    """
+
+    # Function calls to function_1 and function_2.
+    function_1()            # TODO  Remove when you replace it with your function
+    function_2()            # TODO  Remove when you replace it with your function
+
+
+main()  # Starts the program!
