059d608f0dd4940f473fe94970598ebe5ab354a7
feitsopb <feitsopb@berea.edu>
2026-02-11 00:16:55 -0500

    done editing and ready to be intergrated.

--- Answers/HW03_feitsopb.py
+++ Answers/HW03_feitsopb.py
@@ -15,144 +15,152 @@
 
 import turtle
 
+wn = turtle.Screen()
+wn.bgcolor("skyblue")
+t = turtle.Turtle()
+t.speed(7)
+
+
+def lawn():
+    """
+    draws a lawn colored green
+    """
+    t.penup()
+    t.goto(-300, -200)
+    t.pendown()
+    t.color("green")
+    t.begin_fill()
+
+    for i in range(2):
+        t.forward(600)
+        t.left(90)
+        t.forward(120)
+        t.left(90)
+
+    t.end_fill()
+
+def house_base():
+    """
+    draws the structure of the house with a mixture of 2 colors
+    """
+    t.penup()
+    t.goto(-100, -80)  # just above the lawn
+    t.pendown()
+    t.color("black", "burlywood")
+    t.begin_fill()
+
+    for i in range(2):
+        t.forward(200)
+        t.left(90)
+        t.forward(140)
+        t.left(90)
+
+    t.end_fill()
+
+def roof():
+    """
+    draws the structure of the roof
+    """
+    t.penup()
+    t.goto(-100, 60)
+    t.pendown()
+    t.color("black", "brown")
+    t.begin_fill()
+
+    t.forward(200)
+    t.left(135)
+    t.forward(140)
+    t.left(90)
+    t.forward(140)
+    t.left(135)
+
+    t.end_fill()
+
+
+def window_left():
+    """
+    draws the window left
+    """
+    t.color("black", "lightyellow")
+    t.penup()
+    t.goto(-70, -20)
+    t.pendown()
+    t.begin_fill()
+    for i in range(4):
+        t.forward(40)
+        t.left(90)
+    t.end_fill()
+
+def window_right():
+    """
+    draws the window right
+    """
+
+    t.penup()
+    t.goto(30, -20)
+    t.pendown()
+    t.begin_fill()
+    for i in range(4):
+        t.forward(40)
+        t.left(90)
+    t.end_fill()
+
+def door():
+    """
+    draws the door
+    """
+    t.penup()
+    t.goto(-15, -80)  # centered at bottom of house
+    t.pendown()
+    t.color("black", "saddlebrown")
+    t.begin_fill()
+
+    for i in range(2):
+        t.forward(30)
+        t.left(90)
+        t.forward(70)
+        t.left(90)
+
+    t.end_fill()
+
+
+
+
+def sun_rays():
+    """
+    draws the sun with sun rays around it.
+    """
+    t.penup()
+    t.goto(180, 140)
+    t.pendown()
+    t.color("orange", "yellow")
+    t.begin_fill()
+    t.circle(35)
+    t.end_fill()
+
+    # Sun rays
+    for i in range(12):
+        t.penup()
+        t.goto(180, 175)
+        t.setheading(i * 30)
+        t.forward(35)
+        t.pendown()
+        t.forward(15)
+
+def coordinator ():
+    lawn()
+    house_base()
+    roof()
+    window_left()
+    window_right()
+    door()
+    sun_rays()
+
 def main():
     """
     this function draws a house in daylight, with the sun shining Bright, standing on green lawn
     """
-    wn = turtle.Screen()
-    wn.bgcolor("skyblue")
-    t = turtle.Turtle()
-    t.speed(0)
-    def lawn():
-        """
-        draws a lawn colored green
-        """
-        t.penup()
-        t.goto(-300, -200)
-        t.pendown()
-        t.color("green")
-        t.begin_fill()
-
-        for i in range(2):
-            t.forward(600)
-            t.left(90)
-            t.forward(120)
-            t.left(90)
-
-        t.end_fill()
-    lawn()
-
-    def house_base():
-        """
-        draws the structure of the house with a mixture of 2 colors
-        """
-        t.penup()
-        t.goto(-100, -80)   # just above the lawn
-        t.pendown()
-        t.color("black", "burlywood")
-        t.begin_fill()
-
-        for i in range(2):
-            t.forward(200)
-            t.left(90)
-            t.forward(140)
-            t.left(90)
-
-        t.end_fill()
-    house_base()
-
-    def roof():
-        """
-        draws the structure of the roof
-        """
-        t.penup()
-        t.goto(-100, 60)
-        t.pendown()
-        t.color("black", "brown")
-        t.begin_fill()
-
-        t.forward(200)
-        t.left(135)
-        t.forward(140)
-        t.left(90)
-        t.forward(140)
-        t.left(135)
-
-        t.end_fill()
-    roof()
-
-    t.color("black", "lightyellow")
-    def window_left():
-        """
-        draws the window left
-        """
-
-        t.penup()
-        t.goto(-70, -20)
-        t.pendown()
-        t.begin_fill()
-        for i in range(4):
-            t.forward(40)
-            t.left(90)
-        t.end_fill()
-    window_left()
-
-    def window_right():
-        """
-        draws the window right
-        """
-
-        t.penup()
-        t.goto(30, -20)
-        t.pendown()
-        t.begin_fill()
-        for i in range(4):
-            t.forward(40)
-            t.left(90)
-        t.end_fill()
-    window_right()
-
-    def door():
-        """
-        draws the door
-        """
-        t.penup()
-        t.goto(-15, -80)   # centered at bottom of house
-        t.pendown()
-        t.color("black", "saddlebrown")
-        t.begin_fill()
-
-        for i in range(2):
-            t.forward(30)
-            t.left(90)
-            t.forward(70)
-            t.left(90)
-
-        t.end_fill()
-    door()
-
-    def sun_rays():
-        """
-        draws the sun with sun rays around it.
-        """
-        t.penup()
-        t.goto(180, 140)
-        t.pendown()
-        t.color("orange", "yellow")
-        t.begin_fill()
-        t.circle(35)
-        t.end_fill()
-
-        # Sun rays
-        for i in range(12):
-            t.penup()
-            t.goto(180, 175)
-            t.setheading(i * 30)
-            t.forward(35)
-            t.pendown()
-            t.forward(15)
-    sun_rays()
-
+    coordinator()
     t.hideturtle()
     wn.exitonclick()
 main()
