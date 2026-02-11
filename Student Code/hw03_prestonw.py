60ba8f9241a6e3e9d4d3d676378b2523429884ff
Preston Worrix <worrixp@berea.edu>
2026-02-11 09:30:34 -0500

    Fixed according to the comments Dr, Heggen gave me on github

--- Student Code/hw03_prestonw.py
+++ Student Code/hw03_prestonw.py
@@ -12,11 +12,13 @@
 #
 #################################################################################
 
-
 import turtle
 
+def draw_pig_1(turt):
 
-def pig_start(turt):
+    """this function moves the turtle to the propper loctation to start
+    drawing the pig on the screen along with setting the color and pen size"""
+
     turt.speed()
     turt.penup()
     turt.forward(1)
@@ -28,7 +30,11 @@ def pig_start(turt):
     turt.pensize(3)
 
 
-def pig_2(turt):
+def draw_pig_2(turt):
+
+    """ this function draws the first large circle for the pig and fills
+    it in"""
+
     turt.penup()
     turt.forward(120)
     turt.left(90)
@@ -43,6 +49,7 @@ def pig_2(turt):
     turt.left(90)
     turt.forward(120)
     turt.left(90)
+
     turt.pendown()
     turt.color("black")
     turt.fillcolor(255,0,255)
@@ -52,12 +59,17 @@ def pig_2(turt):
         turt.left(1)
     turt.end_fill()
 
-def pig_3(turt):
+def draw_pig_3(turt):
+
+    """ this function draws the semi circles for the ears
+     and fills them in"""
+
     turt.penup()
     for i in range(30):
         turt.forward(3)
         turt.left(1)
     turt.right(90)
+
     turt.pendown()
     turt.fillcolor(255,0,255)
     turt.begin_fill()
@@ -65,12 +77,14 @@ def pig_3(turt):
         turt.forward(1)
         turt.left(1)
     turt.end_fill()
+
     turt.penup()
     turt.left(90)
     for i in range(90):
         turt.forward(3)
         turt.right(1)
     turt.forward(3)
+
     turt.pendown()
     turt.left(90)
     turt.begin_fill()
@@ -78,59 +92,55 @@ def pig_3(turt):
         turt.forward(1)
         turt.right(1)
     turt.end_fill()
-def pig_4(turt):
-    turt.teleport(0,0)
-    turt.left(65)
-    turt.teleport(0,0)
-    turt.speed()
-    turt.penup()
-    turt.forward(1)
-    turt.forward(40)
-    turt.forward(1)
-    turt.color("black")
-    turt.forward(40)
-    turt.pendown()
-    turt.left(90)
-    turt.pensize(3)
-    turt.fillcolor(255,0,255)
-    turt.begin_fill()
-    for i in range(180):
-        turt.left(2)
-        turt.forward(2)
-    turt.end_fill()
 
-def pig_5(turt):
+def draw_pig_4(turt):
+
+    """ this function teleports the turtle back to the 0,0 and draws the face
+    of the pig nose and eyes"""
+
     turt.teleport(0,0)
     turt.left(155)
     turt.color("black")
     turt.pensize(20)
     turt.forward(1)
+
     turt.penup()
     turt.forward(40)
+
     turt.pendown()
     turt.forward(1)
+
     turt.penup()
     turt.forward(40)
     turt.left(90)
+
     turt.pendown()
     turt.pensize(3)
     for i in range(180):
         turt.left(2)
         turt.forward(2)
+
     turt.penup()
     turt.forward(120)
     turt.color("black")
     turt.left(90)
+
     turt.pendown()
     turt.pensize(20)
     turt.forward(1)
+
     turt.penup()
     turt.forward(120)
+
     turt.pendown()
     turt.forward(1)
 
-
 def main():
+
+    """ this function main creates the turtle and screen along with calling
+    all other functions the program in total should use multiple functions
+    to set the background color to green and draw a basic pig colored in
+    using a combination of circles dots and semi circles."""
 
     tess = turtle.Turtle()
     tess.pensize(3)
@@ -139,15 +149,12 @@ def main():
     wn.bgcolor("green")
     wn.colormode(255)
 
-
-
-
-
     # Function calls to function_1 and function_2.
-    pig_start(tess)
-    pig_2(tess)
-    pig_3(tess)
-    pig_5(tess)
+    draw_pig_1(tess)
+    draw_pig_2(tess)
+    draw_pig_3(tess)
+    draw_pig_4(tess)
 
     wn.exitonclick()
+
 main()  # Starts the program!
