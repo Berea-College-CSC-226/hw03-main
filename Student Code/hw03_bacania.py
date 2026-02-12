7fd2f117f9aea9cff093272394cab5c56dcd6019
bright, alicia <aliciabacani07@gmail.com>
2026-02-11 22:49:12 -0500

    to be fixed!

--- Student Code/hw03_bacania.py
+++ Student Code/hw03_bacania.py
@@ -1,34 +1,46 @@
 import turtle
 
-wn = turtle.Screen()
-wn.bgcolor("lightgreen")
-flower_turtle = turtle.Turtle()
-flower_turtle.shape("turtle")
-flower_turtle.color("yellow")
-flower_turtle.speed(0)
-flower_turtle.pensize(30)
-flower_turtle.right(90)
-flower_turtle.forward(1)
+"""
+this code starts us off by centering our
+turtle for further code.
+"""
 
-def drawFlowerStem():
+def drawStart(flower_turtle):
+    flower_turtle.penup()
+    flower_turtle.goto(0, 0)
+    flower_turtle.setheading(90)
+    flower_turtle.pendown()
+
+"""
+this code draws our stem
+"""
+
+def drawFlowerStem(flower_turtle):
     flower_turtle.color("green")
     flower_turtle.pensize(10)
 
     flower_turtle.penup()
-    flower_turtle.goto(0, -0)
+    flower_turtle.goto(0, -250)
     flower_turtle.pendown()
 
     flower_turtle.forward(175)
 
-def drawFlowerLeaves():
+"""
+Because we are drawing a flower, 
+we want our flower to also have small leaves. 
+This code does exactly that.
+"""
+
+def drawFlowerLeaves(flower_turtle):
     flower_turtle.color("green")
     flower_turtle.pensize(10)
 
     flower_turtle.penup()
     flower_turtle.goto(0, 3)
+    flower_turtle.setheading(90)
     flower_turtle.pendown()
 
-    flower_turtle.right(360)
+    flower_turtle.right(180)
     flower_turtle.forward(50)
     flower_turtle.left(90)
     flower_turtle.forward(15)
@@ -43,18 +55,23 @@ def drawFlowerLeaves():
     flower_turtle.left(90)
     flower_turtle.forward(50)
 
-def drawFlowerTurtle():
+"""
+This code is a loop. The reason why I made 
+it into a loop was so that I don't have to 
+keep rewriting turtle functions. 
+"""
+
+def drawFlowerTurtle(flower_turtle):
     for i in ["red", "green", "blue"]:
         flower_turtle.color(i)
         flower_turtle.pensize(10)
-        flower_turtle.penup()
         flower_turtle.left(90)
 
-def drawFlowerPetals():
+def drawFlowerPetals(flower_turtle):
     """
-    please note that this section 
-    was edited by chatgpt with the prompt: 
-    "help me make this to a petal" and is only 
+    please note that this section
+    was edited by chatgpt with the prompt:
+    "help me make this to a petal" and is only
     being kept as a example code.
     """
 
@@ -72,29 +89,23 @@ def drawFlowerPetals():
         flower_turtle.circle(40, 60)
         flower_turtle.left(20)
 
-    # original code
-    # flower_turtle.penup()
-    # flower_turtle.color("red")
-    # for i in range (18):
-    #     flower_turtle.pensize(10)
-    #     flower_turtle.penup()
-    #     flower_turtle.forward(25)
-    #     flower_turtle.pendown()
-    #     flower_turtle.right(180)
-    #     flower_turtle.right(90)
-    #     flower_turtle.forward(10)
-    #     flower_turtle.left(18)
-    #     flower_turtle.forward(10)
-
-
-
 def main():
-    drawFlowerStem()
-    drawFlowerLeaves()
-    drawFlowerTurtle()
-    drawFlowerPetals()
+    wn = turtle.Screen()
+    wn.bgcolor("lightgreen")
+    flower_turtle = turtle.Turtle()
+    flower_turtle.shape("turtle")
+    flower_turtle.speed(0)
+
+
+    drawStart(flower_turtle)
+    drawFlowerStem(flower_turtle)
+    drawFlowerLeaves(flower_turtle)
+    drawFlowerTurtle(flower_turtle)
+    drawFlowerPetals(flower_turtle)
+
+    wn.exitonclick()
 
 main()
-wn.exitonclick()
+
 
 
