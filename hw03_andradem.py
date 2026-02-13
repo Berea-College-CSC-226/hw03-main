fd4dcc901e672e97fac3ce8dea774eb9cc8f9fe9
andradem <andradem@berea.edu>
2026-02-13 02:33:01 -0500

    Rethought everything. Made new functions to better make what I'm doing.

--- hw03_andradem.py
+++ hw03_andradem.py
@@ -30,17 +30,57 @@ def lozenge (turt, thickness, interval, times):
         turt.right(180-angle*.5)
 
 ws = turtle.Screen()
-ws.bgcolor("white")
+ws.bgcolor("darkslategray")
 
 koopa = turtle.Turtle()
+koopa.pensize(10)
 
 # pattern(koopa, 40,10,5)
-koopa.pensize(5)
 
-lozenge(koopa, 25, 50, 1)
-koopa.up()
-koopa.forward(20)
-koopa.down()
-lozenge(koopa, 15, 30, 1)
-ws.exitonclick()
+def unused():
+    lozenge(koopa, 25, 50, 1)
+    koopa.up()
+    koopa.forward(20)
+    koopa.down()
+    lozenge(koopa, 15, 30, 1)
+    ws.exitonclick()
 
+def angleFinder(x,y):
+    return math.atan(y/x)
+
+def leaf (x, y, thickness):
+    koopa.right(angleFinder(x, y))
+    #extraAngle =
+    #adjustAngle =
+    koopa.forward(math.hypot(x, y))
+
+def bostro (turt, x, y):
+    direction = 1
+    for i in range(y//turt.pensize()):
+        turt.fd(x)
+        turt.left(direction*90)
+        turt.up()
+        turt.forward(turt.pensize())
+        turt.down()
+        turt.left(direction*90)
+        direction *= -1
+
+
+def main():
+    # The starter pinapple
+    #koopa.begin_fill()
+    #koopa.forward(-50)
+    #for i in range(0,4):
+    #    koopa.forward(100+i%2*50)
+    #    koopa.right(45)
+    #    koopa.forward(50)
+    #    koopa.right(45)
+
+    #koopa.fillcolor("goldenrod")
+    #koopa.end_fill()
+    bostro(koopa, 50, 50)
+
+
+
+    ws.exitonclick()
+main()
