d1bef5dfb4386c1ca8c72ea0521d1af8900a9a27
bookera <bookera@berea.edu>
2026-09-15 21:12:40 -0400

    Finished Sections 1, 2, and 3, started drawing, finished drawing,

--- hw03_bookera.py
+++ hw03_bookera.py
@@ -9,53 +9,90 @@ import turtle
 #b_legs = turtle.Turtle()
 #b_arms = turtle.Turtle
 #def b_arms(arms):
-    #arms.goto(100,100)
+#arms.goto(100,100)
 
-def b_legs(legs):
-    legs.goto(85,-85)
-    legs.right(45)
-    legs.forward(100)
-    legs.right(120)
-    legs.forward(20)
-    legs.right(45)
-    legs.forward(90)
+def right_side(rs):
+    rs.goto(85,-85)
+    rs.begin_fill()
+    rs.right(45)
+    rs.forward(100)
+    rs.right(120)
+    rs.forward(20)
+    rs.right(50)
+    rs.forward(90)
+    rs.end_fill()
+    rs.penup()
+    rs.left(80)
+    rs.forward(70)
+    rs.pendown()
+    rs.fillcolor("#13598B")
+    rs.begin_fill()
+    rs.left(75)
+    rs.forward(90)
+    rs.left(60)
+    rs.forward(20)
+    rs.left(120)
+    rs.forward(105)
+    rs.hideturtle()
+    rs.end_fill()
 
-
-def b_arms(arms):
-    arms.goto(-85,-85)
-    arms.right(120)
-    arms.forward(100)
-    arms.left(120)
-    arms.forward(20)
-    arms.left(49)
-    arms.forward(80)
-
-
+def left_side(ls):
+    ls.goto(-85,-85)
+    ls.begin_fill()
+    ls.right(120)
+    ls.forward(100)
+    ls.left(120)
+    ls.forward(20)
+    ls.left(49)
+    ls.forward(85)
+    ls.end_fill()
+    ls.penup()
+    ls.right(98)
+    ls.forward(65)
+    ls.pendown()
+    ls.fillcolor("#13598B")
+    ls.begin_fill()
+    ls.right(80)
+    ls.forward(100)
+    ls.left(120)
+    ls.forward(20)
+    ls.left(60)
+    ls.forward(93)
+    ls.hideturtle()
+    ls.end_fill()
 
 def b_chest(chest):
+    chest.begin_fill()
     chest.right(45)
     chest.forward(120)
     for i in range(3):
         chest.right(90)
         chest.forward(120)
+    chest.hideturtle()
+    chest.end_fill()
 
 
 
 
 def b_head(head):
-    head.circle(100)
-
+    head.hideturtle()
+    head.fillcolor("#8B4513")
+    head.begin_fill()
+    head.speed(10)
+    head.circle(80)
+    head.end_fill()
 
 def main():
     wn = turtle.Screen()
+    wn.bgcolor("green")
     head = turtle.Turtle()
     chest = turtle.Turtle()
-    arms = turtle.Turtle()
-    legs = turtle.Turtle()
+    rs = turtle.Turtle()
+    ls = turtle.Turtle()
     b_head(head)
     b_chest(chest)
-    b_arms(arms)
-    b_legs(legs)
+    right_side(rs)
+    left_side(ls)
 
 
 
